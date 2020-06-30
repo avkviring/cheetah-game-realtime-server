b9e6f103bdd4c7b3a3cd63f1c929d7761708a47a
Kviring Aleksey <alex@kviring.com>
2020-06-30 11:34:40 +0500

    Сборка нативной части клиента под Windows, Android и MacOs

---
+++ clients/Rust/make/develop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+OUTPUT=Clients/Unity/Packages/CheetahRelay/
+
+cd ../../../
+
+## macos
+rm -f $OUTPUT/x86_64/libcheetah_relay_client.dylib
+cargo build --manifest-path clients/Rust/Cargo.toml
+mkdir -p $OUTPUT/x86_64/
+cp clients/Rust/target/debug/libcheetah_relay_client.dylib $OUTPUT/x86_64/libcheetah_relay_client.bundle
---
+++ clients/Rust/make/production.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+OUTPUT=Clients/Unity/Packages/CheetahRelay/
+
+cd ../../../
+
+## macos
+rm -f $OUTPUT/x86_64/libcheetah_relay_client.dylib
+cargo build --manifest-path clients/Rust/Cargo.toml  --release --message-format short
+mkdir -p $OUTPUT/x86_64/
+cp clients/Rust/target/release/libcheetah_relay_client.dylib $OUTPUT/x86_64/libcheetah_relay_client.bundle
+
+## android
+rm -f $OUTPUT/Android/librelay.so
+mkdir -p $OUTPUT/Android/
+cross build --manifest-path clients/Rust/Cargo.toml --target armv7-linux-androideabi --release --message-format short
+cp clients/Rust/target/armv7-linux-androideabi/release/libcheetah_relay_client.so $OUTPUT/Android/
+
+## windows
+rm -f $OUTPUT/x86_64/cheetah_relay_client.dll
+cross build --manifest-path clients/Rust/Cargo.toml --target x86_64-pc-windows-gnu --release --message-format short
+cp clients/Rust/target/x86_64-pc-windows-gnu/release/cheetah_relay_client.dll $OUTPUT/x86_64/
