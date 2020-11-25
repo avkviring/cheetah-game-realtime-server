8e4b034e924f3db718a9f3fb4a8835cb09526755
Kviring Aleksey <alex@kviring.com>
2020-11-25 11:41:42 +0500

    [Client] Обновлены dll для платформ

--- clients/Rust/make/build-production-unity-library.sh
+++ clients/Rust/make/build-production-unity-library.sh
@@ -8,7 +8,7 @@ cd ../../../
 rm -f $OUTPUT/x86_64/libcheetah_relay_client.dylib
 cargo build --manifest-path clients/Rust/Cargo.toml  --release --message-format short
 mkdir -p $OUTPUT/x86_64/
-cp clients/Rust/target/release/libcheetah_relay_client.dylib $OUTPUT/x86_64/libcheetah_relay_client.bundle
+cp target/release/libcheetah_relay_client.dylib $OUTPUT/x86_64/libcheetah_relay_client.bundle
 
 # android
 rm -f $OUTPUT/Android/armeabi-v7a/libcheetah_relay_client.so
