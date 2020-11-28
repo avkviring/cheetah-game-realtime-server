2419c8b58b11fc9766bb2b74805c9831eb18f667
Kviring Aleksey <alex@kviring.com>
2020-11-28 07:31:30 +0500

    [Server] Рефакторинг реакции на CTRL+C

--- servers/relay/Server/src/main.rs
+++ servers/relay/Server/src/main.rs
@@ -16,18 +16,11 @@ fn main() {
 
 fn start_server() {
 	let socket = UdpSocket::bind(SocketAddr::from_str("0.0.0.0:5000").unwrap()).unwrap();
-	
 	let server = Server::new(socket, true);
 	let halt_signal = server.get_halt_signal().clone();
-	ctrlc::set_handler(move || {
-		halt_signal.store(true, Ordering::Relaxed);
-	}).expect("Error setting Ctrl-C handler");
-	
 	let server = Arc::new(Mutex::new(server));
 	DumpRestServer::run(server.clone()).join().unwrap().unwrap();
-	
-	let lock_server = &mut server.lock().unwrap();
-	lock_server.join();
+	halt_signal.store(true, Ordering::Relaxed);
 }
 
 
