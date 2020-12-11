c1ba26d9799be6def27eb55055b5efe0df4367cd
Kviring Aleksey <alex@kviring.com>
2020-12-11 13:40:15 +0500

    close 141 Управление уровнем логирования сервера через аргументы командной строки

--- servers/relay/Server/src/main.rs
+++ servers/relay/Server/src/main.rs
@@ -5,19 +5,13 @@ use std::str::FromStr;
 use std::sync::atomic::Ordering;
 use std::sync::{Arc, Mutex};
 
-use clap::{App, Arg, Clap};
+use clap::{App, Arg};
+use log::LevelFilter;
 use stderrlog::Timestamp;
 
 use cheetah_relay::room::template::RoomTemplate;
 use cheetah_relay::server::rest::DumpRestServer;
 use cheetah_relay::server::Server;
-
-#[derive(Clap, Debug)]
-#[clap(version = "1.0", author = "Kevin K. <kbknapp@gmail.com>")]
-struct Opts {
-	#[clap(short, long)]
-	rooms: Vec<String>,
-}
 
 fn main() {
 	let matches = App::new("Cheetah Relay Server")
@@ -32,12 +26,39 @@ fn main() {
 				.about("Config for rooms template (see Server/examples/)")
 				.takes_value(true),
 		)
+		.arg(
+			Arg::new("log-level")
+				.long("log-level")
+				.multiple(false)
+				.short('l')
+				.required(false)
+				.default_value("ERROR")
+				.possible_values(&vec!["TRACE", "DEBUG", "INFO", "WARN", "ERROR"])
+				.about("level for log")
+				.takes_value(true),
+		)
 		.get_matches();
+
+	let level = match matches.values_of("log-level") {
+		None => LevelFilter::Error,
+		Some(log_level) => {
+			let level_opt = log_level.into_iter().next().unwrap();
+			match level_opt {
+				"TRACE" => LevelFilter::Trace,
+				"DEBUG" => LevelFilter::Debug,
+				"INFO" => LevelFilter::Info,
+				"WARN" => LevelFilter::Warn,
+				"ERROR" => LevelFilter::Error,
+				_ => LevelFilter::Error,
+			}
+		}
+	};
+
+	init_logger(level);
 
 	match matches.values_of("room-template") {
 		None => {}
 		Some(room_templates_path) => {
-			init_logger();
 			start_server(room_templates_path);
 		}
 	}
@@ -58,9 +79,9 @@ fn start_server(room_templates_path: clap::Values) {
 	halt_signal.store(true, Ordering::Relaxed);
 }
 
-fn init_logger() {
+fn init_logger(verbosity: LevelFilter) {
 	stderrlog::new()
-		.verbosity(2)
+		.verbosity(verbosity as usize)
 		.show_level(true)
 		.timestamp(Timestamp::Second)
 		.init()
