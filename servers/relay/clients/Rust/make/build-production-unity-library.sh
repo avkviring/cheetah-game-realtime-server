834e53810b9625e6f859c8246b8abfda5ef797ee
Kviring Aleksey <alex@kviring.com>
2020-12-11 08:10:16 +0500

    Исправлен скрипт сборки клиентских библиотек

--- servers/relay/clients/Rust/make/build-production-unity-library.sh
+++ servers/relay/clients/Rust/make/build-production-unity-library.sh
@@ -5,6 +5,7 @@ rustup default stable
 OUTPUT=Clients/Unity/Packages/CheetahRelay/
 
 cd ../../../
+cargo clean
 
 # macos
 rm -f $OUTPUT/x86_64/libcheetah_relay_client.dylib
@@ -13,12 +14,12 @@ mkdir -p $OUTPUT/x86_64/
 cp target/release/libcheetah_relay_client.dylib $OUTPUT/x86_64/libcheetah_relay_client.bundle
 
 # android
-rm -f $OUTPUT/Android/armeabi-v7a/libcheetah_relay_client.so
+rm -f $OUTPUT/Android/armv7-linux-androideabi/libcheetah_relay_client.so
 mkdir -p $OUTPUT/Android/
 cross build --manifest-path clients/Rust/Cargo.toml --target armv7-linux-androideabi --release --message-format short
-cp clients/Rust/target/armv7-linux-androideabi/release/libcheetah_relay_client.so $OUTPUT/Android/
+cp target/armv7-linux-androideabi/release/libcheetah_relay_client.so $OUTPUT/Android/
 
 ## windows
 rm -f $OUTPUT/x86_64/libcheetah_relay_client.dll
 cross build --manifest-path clients/Rust/Cargo.toml --target x86_64-pc-windows-gnu --release --message-format short
-cp clients/Rust/target/x86_64-pc-windows-gnu/release/cheetah_relay_client.dll $OUTPUT/x86_64/libcheetah_relay_client.dll
+cp target/x86_64-pc-windows-gnu/release/cheetah_relay_client.dll $OUTPUT/x86_64/libcheetah_relay_client.dll
