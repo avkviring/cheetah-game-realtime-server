ef2b0a2c14a1ecd68b576b04a24187fef0db8d1a
Kviring Aleksey <alex@kviring.com>
2020-12-18 15:22:55 +0500

    #151 Метод для определения есть ли совместный доступ на запись для объекта

--- servers/relay/Server/src/room/template/permission.rs
+++ servers/relay/Server/src/room/template/permission.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use fnv::FnvBuildHasher;
 use serde::{Deserialize, Serialize};
@@ -14,9 +14,11 @@ pub struct PermissionManager {
 	templates: HashMap<GameObjectTemplateType, Vec<PermissionGroup>, FnvBuildHasher>,
 	fields: HashMap<PermissionFieldKey, Vec<PermissionGroup>, FnvBuildHasher>,
 	cache: HashMap<PermissionCachedFieldKey, Permission, FnvBuildHasher>,
+	write_access_template: HashSet<GameObjectTemplateType, FnvBuildHasher>,
+	write_access_fields: HashSet<PermissionFieldKey, FnvBuildHasher>,
 }
 
-#[derive(Hash, Eq, PartialEq, Debug)]
+#[derive(Hash, Eq, PartialEq, Debug, Clone)]
 struct PermissionFieldKey {
 	template: GameObjectTemplateType,
 	field_id: FieldIdType,
@@ -35,16 +37,27 @@ impl PermissionManager {
 			templates: Default::default(),
 			fields: Default::default(),
 			cache: Default::default(),
+			write_access_template: Default::default(),
+			write_access_fields: Default::default(),
 		};
 
 		for template in &permission.templates {
+			if template.groups.iter().find(|t| t.permission > Permission::Ro).is_some() {
+				result.write_access_template.insert(template.template);
+			}
 			result.templates.insert(template.template.clone(), template.groups.clone());
+
 			for field in &template.fields {
 				let key = PermissionFieldKey {
 					template: template.template,
 					field_id: field.field_id,
 					field_type: field.field_type.clone(),
 				};
+
+				if field.groups.iter().find(|t| t.permission > Permission::Ro).is_some() {
+					result.write_access_fields.insert(key.clone());
+				}
+
 				result.fields.insert(key, field.groups.clone());
 			}
 		}
@@ -56,7 +69,12 @@ impl PermissionManager {
 	/// Доступен ли объект на запись другим пользователем кроме создателя
 	///
 	pub fn has_write_access(&mut self, template: GameObjectTemplateType, field_id: FieldIdType, field_type: FieldType) -> bool {
-		unimplemented!()
+		self.write_access_template.contains(&template)
+			|| self.write_access_fields.contains(&PermissionFieldKey {
+				template,
+				field_id,
+				field_type,
+			})
 	}
 
 	pub fn get_permission(
@@ -223,5 +241,61 @@ mod tests {
 			permissions_manager.get_permission(10, 15, FieldType::Long, AccessGroups(0b01)),
 			Permission::Rw
 		);
+	}
+
+	#[test]
+	fn should_not_has_write_access_by_default() {
+		let mut permissions = Permissions::default();
+		let mut permissions_manager = PermissionManager::new(&permissions);
+		assert!(!permissions_manager.has_write_access(10, 100, FieldType::Long));
+	}
+
+	#[test]
+	fn should_has_write_access_if_object_has_write_permission() {
+		let mut permissions = Permissions::default();
+		permissions.templates.push(TemplatePermission {
+			template: 10,
+			groups: vec![PermissionGroup {
+				group: Default::default(),
+				permission: Permission::Rw,
+			}],
+			fields: vec![],
+		});
+		let mut permissions_manager = PermissionManager::new(&permissions);
+		assert!(permissions_manager.has_write_access(10, 100, FieldType::Long));
+	}
+
+	#[test]
+	fn should_not_has_write_access_if_object_has_read_permission() {
+		let mut permissions = Permissions::default();
+		permissions.templates.push(TemplatePermission {
+			template: 10,
+			groups: vec![PermissionGroup {
+				group: Default::default(),
+				permission: Permission::Ro,
+			}],
+			fields: vec![],
+		});
+		let mut permissions_manager = PermissionManager::new(&permissions);
+		assert!(!permissions_manager.has_write_access(10, 100, FieldType::Long));
+	}
+
+	#[test]
+	fn should_has_write_access_if_object_has_field_with_write_permission() {
+		let mut permissions = Permissions::default();
+		permissions.templates.push(TemplatePermission {
+			template: 10,
+			groups: vec![],
+			fields: vec![PermissionField {
+				field_id: 100,
+				field_type: FieldType::Long,
+				groups: vec![PermissionGroup {
+					group: Default::default(),
+					permission: Permission::Rw,
+				}],
+			}],
+		});
+		let mut permissions_manager = PermissionManager::new(&permissions);
+		assert!(permissions_manager.has_write_access(10, 100, FieldType::Long));
 	}
 }
