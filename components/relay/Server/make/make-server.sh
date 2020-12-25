7968cb40ecd731db642bfcba1046d63d33dd8615
Kviring Aleksey <alex@kviring.com>
2020-12-25 11:40:48 +0500

    Обновление и перезапуск хостинга

--- components/relay/Server/make/make-server.sh
+++ components/relay/Server/make/make-server.sh
@@ -2,4 +2,10 @@
 
 cd ../../
 cross build --manifest-path Server/Cargo.toml --target x86_64-unknown-linux-gnu --release --message-format short
-scp  target/x86_64-unknown-linux-gnu/release/cheetah_relay root@167.172.138.215:/relay/server
+rsync -v target/x86_64-unknown-linux-gnu/release/cheetah_relay root@167.172.138.215:/relay/server
+rsync -v target/x86_64-unknown-linux-gnu/release/cheetah_relay root@178.62.194.212:/relay/server
+ssh root@167.172.138.215 -C pkill -9 server
+ssh root@178.62.194.212 -C pkill -9 server
+sleep 2
+curl -Is http://167.172.138.215:8080/dump |head -n 1
+curl -Is http://167.172.138.212:8080/dump |head -n 1
