eea4601db0498739611ab8abe1bee811c801608d
Kviring Aleksey <alex@kviring.com>
2021-03-12 08:06:20 +0500

    docs: Обновлен README.md

--- docs/public/publish.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -rf site
-docker run --rm -it -p 8000:8000 -v "${PWD}":/docs squidfunk/mkdocs-material build -f public-mkdocs.yml
-rsync -var site/ root@docs.cheetah.games:/var/www/docs.cheetah.games/html/
