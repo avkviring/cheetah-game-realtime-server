de39b86ab870e9a93b6fa7dc79bf9164bfe26c97
Kviring Aleksey <alex@kviring.com>
2021-03-19 07:46:25 +0500

    refactor(unity): Единый Unity проект для всех модулей.

--- clients/Unity/Assets/Scripts/Auth/AndroidAuth.cs
+++ clients/Unity/Assets/Scripts/Auth/AndroidAuth.cs
@@ -26,37 +26,37 @@ namespace games.cheetah.auth
 
             first = false;
 
-            Debug.Log("AndroidAuth Starting auth");
-            PlayGamesClientConfiguration config =
-                new PlayGamesClientConfiguration.Builder("663521173650-gkgrl7aouifjag0j5do14pul1hdqvosm.apps.googleusercontent.com")
-                    .RequestIdToken()
-                    .Build();
-            PlayGamesPlatform.DebugLogEnabled = true;
-            PlayGamesPlatform.InitializeInstance(config);
-            PlayGamesPlatform.Activate();            
-            
-            Debug.Log("AndroidAuth Runtime platform "+Application.platform);
-            
-            PlayGamesPlatform.Instance.Authenticate(SignInInteractivity.CanPromptOnce, (result) =>
-            {
+            // Debug.Log("AndroidAuth Starting auth");
+            // PlayGamesClientConfiguration config =
+            //     new PlayGamesClientConfiguration.Builder("663521173650-gkgrl7aouifjag0j5do14pul1hdqvosm.apps.googleusercontent.com")
+            //         .RequestIdToken()
+            //         .Build();
+            // PlayGamesPlatform.DebugLogEnabled = true;
+            // PlayGamesPlatform.InitializeInstance(config);
+            // PlayGamesPlatform.Activate();            
+            //
+            // Debug.Log("AndroidAuth Runtime platform "+Application.platform);
+            //
+            // PlayGamesPlatform.Instance.Authenticate(SignInInteractivity.CanPromptOnce, (result) =>
+            // {
+            //
+            //     googleToken.text = PlayGamesPlatform.Instance.GetIdToken();
+            //     Debug.Log("AndroidAuth Authenticate " + result);
+            //     Debug.Log("AndroidAuth Token Id " + PlayGamesPlatform.Instance.GetIdToken());
+            //     Debug.Log("AndroidAuth Token User Id " + PlayGamesPlatform.Instance.GetUserId());
+            //     Debug.Log("AndroidAuth Token Display Name " + PlayGamesPlatform.Instance.GetUserDisplayName());
+            //     
+            //     var channel = new Channel("192.168.212.97:6200", ChannelCredentials.Insecure);
+            //     var client = new Games.Cheetah.Auth.External.Google.Google.GoogleClient(channel);
+            //     var registryOrLoginRequest = new RegistryOrLoginRequest();
+            //     registryOrLoginRequest.DeviceId = "device-id";
+            //     registryOrLoginRequest.GoogleToken = PlayGamesPlatform.Instance.GetIdToken();
+            //     var tokens = client.RegistryOrLogin(registryOrLoginRequest);
+            //     Debug.Log("Session token "+tokens.Session);
+            //     cerberusToken.text = tokens.Session;
 
-                googleToken.text = PlayGamesPlatform.Instance.GetIdToken();
-                Debug.Log("AndroidAuth Authenticate " + result);
-                Debug.Log("AndroidAuth Token Id " + PlayGamesPlatform.Instance.GetIdToken());
-                Debug.Log("AndroidAuth Token User Id " + PlayGamesPlatform.Instance.GetUserId());
-                Debug.Log("AndroidAuth Token Display Name " + PlayGamesPlatform.Instance.GetUserDisplayName());
-                
-                var channel = new Channel("192.168.212.97:6200", ChannelCredentials.Insecure);
-                var client = new Games.Cheetah.Auth.External.Google.Google.GoogleClient(channel);
-                var registryOrLoginRequest = new RegistryOrLoginRequest();
-                registryOrLoginRequest.DeviceId = "device-id";
-                registryOrLoginRequest.GoogleToken = PlayGamesPlatform.Instance.GetIdToken();
-                var tokens = client.RegistryOrLogin(registryOrLoginRequest);
-                Debug.Log("Session token "+tokens.Session);
-                cerberusToken.text = tokens.Session;
 
-
-            });
+//            });
         }
     }
 }
