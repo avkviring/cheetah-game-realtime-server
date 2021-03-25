1d4227cc5bd06840f7880dc6093bf3a3d0b4ab5a
Kviring Aleksey <alex@kviring.com>
2021-03-25 11:41:28 +0500

    feat(relay): Скрипт для создания локальных клиентских библиотек.

--- server/relay/Client/make/build-develop-unity-library.sh
+++ server/relay/Client/make/build-develop-unity-library.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-rustup default stable
-
-cd ../../../
-
+cd ../../
+OUTPUT="../../clients/Unity/Packages/games.cheetah.relay/"
 ## macos
 rm -f $OUTPUT/x86_64/libcheetah_relay_client.bundle
-cargo build --manifest-path clients/Rust/Cargo.toml --release
-mkdir -p 'Clients/Unity/Packages/Cheetah Relay/x86_64/'
-cp target/release/libcheetah_relay_client.dylib 'Clients/Unity/Packages/Cheetah Relay/x86_64/libcheetah_relay_client.bundle'
+cargo build --manifest-path Client/Cargo.toml --release
+cp target/release/libcheetah_relay_client.dylib "$OUTPUT/x86_64/libcheetah_relay_client.bundle"
 
+## armv7
+cross build --manifest-path Client/Cargo.toml --target armv7-linux-androideabi --release --message-format short
+cp target/armv7-linux-androideabi/release/libcheetah_relay_client.so "$OUTPUT/Android/armv7/libcheetah_relay_client.so"
+
+## armv64
+cross build --manifest-path Client/Cargo.toml --target armv7-linux-androideabi --release --message-format short
+cp target/aarch64-linux-android/release/libcheetah_relay_client.so "$OUTPUT/Android/aarch64/libcheetah_relay_client.so"
