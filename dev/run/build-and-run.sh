23a6c063fc389f9332c143ae077848dc4193cc1b
Kviring Aleksey <alex@kviring.com>
2021-05-20 11:26:33 +0500

    refactor(server): Приборка в системе запуского локального сервера

--- dev/run/build-and-run.sh
+++ dev/run/build-and-run.sh
@@ -2,4 +2,5 @@
 set -e
 export COMPOSE_DOCKER_CLI_BUILD=1
 export DOCKER_BUILDKIT=1
-docker-compose --env-file env.platform up --build --abort-on-container-exit --remove-orphans
+docker-compose up --build --abort-on-container-exit --remove-orphans
+
