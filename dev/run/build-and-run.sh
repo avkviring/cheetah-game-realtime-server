a6df62d4ced18033cdff90117bfc9ee3f28f4b52
Kviring Aleksey <alex@kviring.com>
2021-06-01 15:00:42 +0500

    feat(docker): Переход на alpine для уменьшения размеров готовых образов. Рефакторинг dockerfile.

--- dev/run/build-and-run.sh
+++ dev/run/build-and-run.sh
@@ -2,5 +2,5 @@
 set -e
 export COMPOSE_DOCKER_CLI_BUILD=1
 export DOCKER_BUILDKIT=1
-docker-compose up --build --abort-on-container-exit --remove-orphans
+docker compose up --build --abort-on-container-exit --remove-orphans
 
