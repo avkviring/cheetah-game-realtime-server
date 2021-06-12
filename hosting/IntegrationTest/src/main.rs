1374e400f8768d645ca53770e9d4db6771e7ca4e
Kviring Aleksey <alex@kviring.com>
2021-06-12 08:06:20 +0500

    test(hosting): В интеграционный тест добавлен cerberus

--- hosting/IntegrationTest/src/main.rs
+++ hosting/IntegrationTest/src/main.rs
@@ -1,7 +1,10 @@
 use tonic::transport::*;
-use tonic::{Request, Response, Status};
+use tonic::Request;
 
+use cerberus_client::CerberusClient;
 use proto::authentication::external::cookie;
+use proto::cerberus::external::{cerberus_client, RefreshTokenRequest};
+use proto::cerberus::types::Tokens;
 
 pub mod proto;
 
@@ -9,23 +12,28 @@ pub mod proto;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let url = "https://test.dev.cheetah.games";
     let channel = Channel::from_static(url).connect().await?;
-
-    let mut client = cookie::cookie_client::CookieClient::new(channel);
-
-    let response = client
-        .register(Request::new(cookie::RegistryRequest {
-            device_id: "device-id".to_owned(),
-        }))
-        .await;
-
-    match response {
-        Ok(response) => {
-            println!("response cookie {}", response.get_ref().cookie);
-        }
-        Err(err) => {
-            println!("response error {}", err.message());
-        }
-    }
-
+    let tokens = test_authentication_service(channel.clone()).await;
+    test_cerberus_service(channel.clone(), tokens).await;
     Result::Ok(())
+}
+
+async fn test_cerberus_service(channel: Channel, tokens: Tokens) {
+    let mut client = CerberusClient::new(channel);
+    let request = Request::new(RefreshTokenRequest {
+        token: tokens.refresh,
+    });
+    let response = client.refresh(request).await;
+    response.expect("cerberus service error");
+    println!("Test cerberus ... OK");
+}
+
+async fn test_authentication_service(channel: Channel) -> Tokens {
+    let mut client = cookie::cookie_client::CookieClient::new(channel);
+    let request = Request::new(cookie::RegistryRequest {
+        device_id: "device-id".to_owned(),
+    });
+    let response = client.register(request).await;
+    let response = response.expect("authentication service error");
+    println!("Test authentication ... OK");
+    return response.get_ref().tokens.clone().unwrap();
 }
