1dbd5c4fc5d26da56021ce29dfd5b956d1fe910e
Kviring Aleksey <alex@kviring.com>
2021-06-12 08:04:37 +0500

    feat(hosting/cerberus): Подключен внешний GRPC.

--- server/cerberus/Service/src/main.rs
+++ server/cerberus/Service/src/main.rs
@@ -7,30 +7,22 @@ pub mod service;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     pretty_env_logger::init();
-    log::info!("start cerberus");
+    println!("±± cheetah game cerberus component ±±");
 
     // ключи для генерации токенов
     let jwt_public_key = get_key_from_env("JWT_PUBLIC_KEY");
     let jwt_private_key = get_key_from_env("JWT_PRIVATE_KEY");
 
     // параметры redis
-    let redis_host = std::env::var("REDIS_HOST").unwrap_or("cerberus_redis".to_owned());
-    let redis_auth = std::env::var("REDIS_AUTH").ok();
-    let redis_port = std::env::var("REDIS_PORT")
-        .unwrap_or("6379".to_owned())
+    let redis_host = get_env("CERBERUS_REDIS_MASTER_SERVICE_HOST");
+    let redis_port = get_env("CERBERUS_REDIS_MASTER_SERVICE_PORT")
         .parse()
         .unwrap();
+    let redis_auth = std::env::var("REDIS_AUTH").ok();
 
     // порты grpc сервисов
-    let internal_service_port = std::env::var("INTERNAL_GRPC_SERVICE_PORT")
-        .unwrap_or("5001".to_owned())
-        .parse()
-        .unwrap();
-
-    let external_service_port = std::env::var("EXTERNAL_GRPC_SERVICE_PORT")
-        .unwrap_or("5000".to_owned())
-        .parse()
-        .unwrap();
+    let external_service_port = 5000;
+    let internal_service_port = 5001;
 
     run_grpc_server(
         jwt_public_key,
