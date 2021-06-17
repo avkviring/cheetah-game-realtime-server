3ad25c8272621bc02c8618607625f3a44462c00c
Kviring Aleksey <alex@kviring.com>
2021-06-17 08:45:27 +0500

    chore(jwt): Отказ от использование base64 для кодирования JWT ключей.

--- server/cerberus/Service/src/main.rs
+++ server/cerberus/Service/src/main.rs
@@ -10,8 +10,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("±± cheetah game cerberus component ±±");
 
     // ключи для генерации токенов
-    let jwt_public_key = get_key_from_env("JWT_PUBLIC_KEY");
-    let jwt_private_key = get_key_from_env("JWT_PRIVATE_KEY");
+    let jwt_public_key = get_env("JWT_PUBLIC_KEY");
+    let jwt_private_key = get_env("JWT_PRIVATE_KEY");
 
     // параметры redis
     let redis_host = get_env("REDIS_HOST");
@@ -34,11 +34,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     .await;
     Ok(())
 }
-fn get_key_from_env(name: &str) -> String {
-    let value = get_env(name);
-    String::from_utf8(base64::decode(value).unwrap()).unwrap()
-}
-
 fn get_env(name: &str) -> String {
     let value = std::env::var(name).unwrap_or_else(|_| panic!("Env {}", name));
     if value.trim().is_empty() {
