ac31da1a704d4e072ec654aa894a55c806bddc1e
Kviring Aleksey <alex@kviring.com>
2021-06-29 10:33:54 +0500

    chore(server/Microservice): Общие утилитные зависимости для микросервисов.

---
+++ server/Microservice/src/lib.rs
@@ -0,0 +1,18 @@
+use std::net::SocketAddr;
+
+pub fn get_env(name: &str) -> String {
+    let value = std::env::var(name).unwrap_or_else(|_| panic!("Env {}", name));
+    if value.trim().is_empty() {
+        panic!("Env {} is empty", name);
+    }
+    value
+}
+
+pub fn init(name: &str) {
+    pretty_env_logger::init();
+    println!("start service {} ", name);
+}
+
+pub fn get_internal_grpc_address() -> SocketAddr {
+    "0.0.0.0:5001".parse().unwrap()
+}
