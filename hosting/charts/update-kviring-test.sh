63ef08737c23fba161da52ece8d381b9959be448
Kviring Aleksey <alex@kviring.com>
2021-09-21 11:17:21 +0500

    feat(server/match/Factory): Запуск factory с новой конфигурацией в kubernetes.

--- hosting/charts/update-kviring-test.sh
+++ hosting/charts/update-kviring-test.sh
@@ -1,3 +1,5 @@
+
+tar -czf Platform/charts/matches-factory/rooms-configuration.tgz -C ../../server/matches/Factory/example/ .
 helm -n kviring uninstall kviring
 kubectl delete --namespace kviring --all deployments,statefulsets,services,pods,pvc,pv
 helm -n kviring upgrade --install kviring Platform -f Platform/values-dev.yaml
