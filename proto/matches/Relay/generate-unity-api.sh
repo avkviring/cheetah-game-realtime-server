eab8a07d858f98b407c538c0b768e43d923af57b
Kviring Aleksey <alex@kviring.com>
2021-10-07 12:57:42 +0500

    feat(Трассировка сетевых команд в Unity - #476): Генератор gRPC для Unity.

---
+++ proto/matches/Relay/generate-unity-api.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+project_dir=$(pwd)/../../../
+
+docker run --rm -v$project_dir:/tmp/source -w /tmp/source/proto/matches/Relay akviring/protoc:latest \
+  protoc \
+  --proto_path=. \
+  --plugin=protoc-gen-grpc=/usr/bin/grpc_csharp_plugin \
+  --grpc_out=/tmp/source/clients/Unity/Packages/games.cheetah.matches.relay/Editor/NetworkCommandsViewer/GRPC \
+  --csharp_out=/tmp/source/clients/Unity/Packages/games.cheetah.matches.relay/Editor/NetworkCommandsViewer/GRPC \
+  matches.relay.admin.proto
