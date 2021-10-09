977cd87fa470e0d43b950786875e7031d18308cb
Kviring Aleksey <alex@kviring.com>
2021-10-09 09:00:03 +0500

    feat(clients/Unity): Поддержка admin grpc сервисов в системе запуска локального сервера.

--- server/matches/Relay/Client/make/build-develop-unity-library.sh
+++ server/matches/Relay/Client/make/build-develop-unity-library.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd ../../
-OUTPUT="../../clients/Unity/Packages/games.cheetah.relay/"
+OUTPUT="../../../clients/Unity/Packages/games.cheetah.matches.relay/"
 ## macos
 rm -f $OUTPUT/x86_64/libcheetah_relay_client.bundle
 cargo build --manifest-path Client/Cargo.toml --release
-cp target/release/libcheetah_relay_client.dylib "$OUTPUT/x86_64/libcheetah_relay_client.bundle"
+pwd
+cp ../../target/release/libcheetah_matches_relay_client.dylib "$OUTPUT/x86_64/libcheetah_relay_client.bundle"
