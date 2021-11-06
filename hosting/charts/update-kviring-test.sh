100f091fa91579d82f59fe54dfc642f205ad49e8
Kviring Aleksey <alex@kviring.com>
2021-11-06 17:02:06 +0300

    refactor(helm): Убрана версия образов по-умолчанию, так как это маскировало ошибку установки версии из ci.

--- hosting/charts/update-kviring-test.sh
+++ hosting/charts/update-kviring-test.sh
@@ -2,5 +2,9 @@
 tar -czf Platform/charts/matches-factory/rooms-configuration.tgz -C ../../server/matches/Factory/example/ .
 helm -n kviring uninstall kviring
 kubectl delete --namespace kviring --all deployments,statefulsets,services,pods,pvc,pv
-helm -n kviring upgrade --install kviring Platform -f Platform/values-dev.yaml --set global.grpcDomain=kviring.cluster.dev.cheetah.games
+helm -n kviring upgrade \
+--install kviring Platform \
+-f Platform/values-dev.yaml \
+--set global.grpcDomain=kviring.cluster.dev.cheetah.games \
+--set global.platformImageVersion=999.999.999
 
