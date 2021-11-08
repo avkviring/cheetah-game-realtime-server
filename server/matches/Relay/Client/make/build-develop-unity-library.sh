534fc9d5f92f4611124c88809aaf41fcf7129d8e
Kviring Aleksey <alex@kviring.com>
2021-11-08 16:30:09 +0300

    chore(server/matches/Relay/Client): Скрипт для сборки тестового клиента для локальной отладки.

--- server/matches/Relay/Client/make/build-develop-unity-library.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ../../
-OUTPUT="../../../clients/Unity/Packages/games.cheetah.matches.relay/"
-## macos
-rm -f $OUTPUT/x86_64/libcheetah_relay_client.bundle
-cargo build --manifest-path Client/Cargo.toml --release
-pwd
-cp ../../target/release/libcheetah_matches_relay_client.dylib "$OUTPUT/x86_64/libcheetah_relay_client.bundle"
---
+++ server/matches/Relay/build-mac-os-client-relay.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## сборка relay клиента для тестирования на macos
+OUTPUT="../../../clients/Unity/Packages/games.cheetah.matches.relay/"
+## macos
+rm -f $OUTPUT/x86_64/libcheetah_matches_relay_client.bundle
+cargo build --manifest-path Client/Cargo.toml
+pwd
+cp ../../target/debug/libcheetah_matches_relay_client.dylib "$OUTPUT/x86_64/libcheetah_matches_relay_client.bundle"
