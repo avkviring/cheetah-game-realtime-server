4e6fa1f590a20d45203f706976a64972897a3092
Kviring Aleksey <alex@kviring.com>
2021-11-12 18:53:54 +0300

    refactor(hosting): Скрипт локальной выкладки helm теперь принимает имя namespace.

---
+++ hosting/charts/install.sh
@@ -0,0 +1,10 @@
+## Выкладка платформы в kubernetes для локальных тестов
+tar -czf Platform/charts/matches-factory/rooms-configuration.tgz -C ../../server/matches/Factory/example/ .
+helm -n kviring uninstall $1
+kubectl delete --namespace $1 --all deployments,statefulsets,services,pods,pvc,pv
+helm -n $1 upgrade \
+--install $1 Platform \
+-f Platform/values-dev.yaml \
+--set global.grpcDomain=$1.cluster.dev.cheetah.games \
+--set global.platformImageVersion=999.999.999
+
