e040e7418d2a45e9d1112301066504d58f4365a5
Kviring Aleksey <alex@kviring.com>
2022-02-10 06:59:00 +0500

    feat(Объединить все микросервисы регистрации пользователей в один #783): Helm для нового сервиса.

--- hosting/charts/install.sh
+++ hosting/charts/install.sh
@@ -1,10 +1,10 @@
 ## Выкладка платформы в kubernetes для локальных тестов
-tar -czf Platform/charts/matches-factory/rooms-configuration.tgz -C ../../server/matches/Factory/example/ .
+tar -czf rooms-configuration.tgz -C ../../server/matches/Factory/example/ .
 helm -n $1 uninstall $1
 kubectl delete --namespace $1 --all deployments,statefulsets,services,pods,pvc,pv
 helm -n $1 upgrade \
 --install $1 Platform \
 -f Platform/values-dev.yaml \
 --set global.grpcDomain=$1.cluster.dev.cheetah.games \
---set global.platformImageVersion=999.999.999
-
+--set global.platformImageVersion=999.999.999 \
+--set global.roomsConfiguration=`cat rooms-configuration.tgz | openssl enc -A -base64` \
