8541b0f1f1aa40798caa74320cbfcaeaac2da0cf
Kviring Aleksey <alex@kviring.com>
2022-02-11 07:27:30 +0500

    feat(Объединить все микросервисы регистрации пользователей в один #783): Миграция sql при создании пула.

--- server/Accounts/src/postgresql.rs
+++ server/Accounts/src/postgresql.rs
@@ -4,16 +4,14 @@ use sqlx::PgPool;
 pub async fn create_postgres_pool(db: &str, user: &str, passwd: &str, host: &str, port: u16) -> PgPool {
 	use std::time::Duration;
 	let uri = format!("postgres://{}:{}@{}:{}/{}", user, passwd, host, port, db);
-	PgPoolOptions::new()
+	let pool = PgPoolOptions::new()
 		.max_connections(5)
 		.connect_timeout(Duration::from_secs(10))
 		.connect(&uri)
 		.await
-		.unwrap()
-}
-
-pub async fn migrate_db(pool: &PgPool) {
-	sqlx::migrate!().run(pool).await.unwrap();
+		.unwrap();
+	sqlx::migrate!().run(&pool).await.unwrap();
+	pool
 }
 
 #[cfg(test)]
@@ -24,7 +22,7 @@ pub mod test {
 	use testcontainers::images::postgres::Postgres;
 	use testcontainers::{Container, Docker};
 
-	use crate::postgresql::{create_postgres_pool, migrate_db};
+	use crate::postgresql::{create_postgres_pool};
 	use crate::PgPool;
 
 	pub async fn setup_postgresql_storage(cli: &Cli) -> (PgPool, Container<'_, Cli, Postgres>) {
@@ -35,7 +33,6 @@ pub mod test {
 		let node = cli.run(image);
 		let port = node.get_host_port(5432).unwrap();
 		let pool = create_postgres_pool("authentication", "authentication", "passwd", "127.0.0.1", port).await;
-		migrate_db(&pool).await;
 		(pool, node)
 	}
 }
