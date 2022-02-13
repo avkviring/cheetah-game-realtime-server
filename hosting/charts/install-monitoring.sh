8df6100de67261ce3e5b608070136ad2c28a76e7
Kviring Aleksey <alex@kviring.com>
2022-02-13 07:08:03 +0500

    feat(hosting/monitoring): Развертывание prometheus & grafana.

---
+++ hosting/charts/install-monitoring.sh
@@ -0,0 +1,11 @@
+## Обновление/инсталляция сервисов для мониторинга
+## параметр - имя домена, пароль администратора для grafana, пароль для администрирования prometheus
+helm -n monitoring upgrade --install monitoring Monitoring \
+  --set global.prometheusAdminAuth=$(echo $3 | htpasswd -n -i admin | openssl enc -A -base64) \
+  --set kube-prometheus-stack.prometheus.ingress.hosts[0]=prometheus.$1 \
+  --set kube-prometheus-stack.prometheus.ingress.tls[0].hosts=\{prometheus.$1\} \
+  --set kube-prometheus-stack.prometheus.ingress.tls[0].secretName=prometheus-ingress \
+  --set kube-prometheus-stack.grafana.ingress.hosts[0]=grafana.$1 \
+  --set kube-prometheus-stack.grafana.ingress.tls[0].hosts=\{grafana.$1\} \
+  --set kube-prometheus-stack.grafana.ingress.tls[0].secretName=grafana-ingress \
+  --set kube-prometheus-stack.grafana.adminPassword=$2
