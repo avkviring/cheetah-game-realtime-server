d55d3d6568e4769628f6cadcb2f519231199899d
Kviring Aleksey <alex@kviring.com>
2022-02-17 06:22:59 +0500

    ci: pv в Kubernetes не имеет namespace и поэтому его нельзя удалять при подготовки конкретного namespace для интеграционного теста.

--- hosting/charts/install-platform.sh
+++ hosting/charts/install-platform.sh
@@ -1,7 +1,7 @@
 ## Выкладка платформы в kubernetes для локальных тестов
 tar -czf rooms-configuration.tgz -C ../../server/matches/Factory/example/ .
 helm -n $1 uninstall $1
-kubectl delete --namespace $1 --all deployments,statefulsets,services,pods,pvc,pv
+kubectl delete --namespace $1 --all deployments,statefulsets,services,pods,pvc
 helm -n $1 upgrade \
 --install $1 Platform \
 -f Platform/values-dev.yaml \
