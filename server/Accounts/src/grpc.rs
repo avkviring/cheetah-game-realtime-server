6c1a757caad8f2926519625acc1bd2ace0e90dc6
Kviring Aleksey <alex@kviring.com>
2022-02-22 13:05:34 +0500

    feat(Добавить в сервисы grpc health checking #391): Добавлено в Accounts.

--- server/Accounts/src/grpc.rs
+++ server/Accounts/src/grpc.rs
@@ -1,11 +1,16 @@
 use jsonwebtoken_google::Parser;
+use tokio::sync::watch;
 use tonic::transport::Server;
+use tonic_health::ServingStatus;
 
 use cheetah_microservice::jwt::JWTTokenParser;
 
 use crate::cookie::CookieGrpcService;
 use crate::google::storage::GoogleStorage;
 use crate::google::GoogleGrpcService;
+use crate::proto::cookie_server::CookieServer;
+use crate::proto::google_server::GoogleServer;
+use crate::proto::tokens_server::TokensServer;
 use crate::tokens::grpc::TokensGrpcService;
 use crate::tokens::TokensService;
 use crate::users::UserService;
@@ -20,6 +25,9 @@ pub async fn run_grpc_server(
 	pool: PgPool,
 	google_client_id: Option<String>,
 ) {
+	let (mut health_reporter, health_service) = tonic_health::server::health_reporter();
+
+	health_reporter.set_service_status("", ServingStatus::Serving).await;
 	let token_service = TokensService::new(jwt_private_key, jwt_public_key.clone(), redis_host, redis_port, redis_auth).await;
 	let user_service = UserService::new(pool.clone());
 
@@ -30,10 +38,16 @@ pub async fn run_grpc_server(
 		user_service.clone(),
 	));
 
+	// если мы здесь - то соединение к базе установлены, все параметры заданы
+	// то есть мы можем сказать что сервисы тоже готовы
+	health_reporter.set_serving::<TokensServer<TokensGrpcService>>().await;
+	health_reporter.set_serving::<CookieServer<CookieGrpcService>>().await;
+
 	let external_addr = cheetah_microservice::get_external_service_binding_addr();
 
 	let builder = Server::builder()
 		.accept_http1(true)
+		.add_service(tonic_web::enable(health_service))
 		.add_service(tonic_web::enable(token_grpc_service))
 		.add_service(tonic_web::enable(cookie_grpc_service));
 
@@ -45,6 +59,8 @@ pub async fn run_grpc_server(
 			Parser::new(&google_client_id),
 			JWTTokenParser::new(jwt_public_key),
 		));
+
+		health_reporter.set_serving::<GoogleServer<GoogleGrpcService>>().await;
 		let builder = builder.add_service(tonic_web::enable(google_grpc_service));
 		builder.serve(external_addr).await.unwrap();
 	} else {
