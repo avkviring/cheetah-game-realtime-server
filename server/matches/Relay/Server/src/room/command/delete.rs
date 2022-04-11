563f8963679013683668947b2c0f9fa368fa9fc3
Kviring Aleksey <alex@kviring.com>
2022-04-11 20:39:26 +0500

    feat(server/matches/Relay): Удаление объекта с клиента без проверки прав доступа.

--- server/matches/Relay/Server/src/room/command/delete.rs
+++ server/matches/Relay/Server/src/room/command/delete.rs
@@ -7,15 +7,6 @@ use crate::room::Room;
 
 impl ServerCommandExecutor for DeleteGameObjectCommand {
 	fn execute(&self, room: &mut Room, member_id: RoomMemberId) -> Result<(), ServerCommandError> {
-		let member = room.get_member(&member_id)?;
-		if let GameObjectOwner::Member(object_id_user) = self.object_id.owner {
-			if object_id_user != member.id {
-				return Err(ServerCommandError::MemberNotOwnerGameObject {
-					object_id: self.object_id.clone(),
-					member_id,
-				});
-			}
-		}
 		room.delete_object(&self.object_id)?;
 		Ok(())
 	}
@@ -54,30 +45,5 @@ mod tests {
 		assert!(matches!(room.get_object_mut(&object_id), Err(_)));
 		assert!(matches!(room.test_get_user_out_commands(user_a_id).pop_back(), None));
 		assert!(matches!(room.test_get_user_out_commands(user_b_id).pop_back(), Some(S2CCommand::Delete(c)) if c==command));
-	}
-
-	#[test]
-	fn should_not_delete_if_not_owner() {
-		let template = RoomTemplate::default();
-		let access_groups = AccessGroups(55);
-		let mut room = Room::from_template(template);
-		let user_a = room.register_member(MemberTemplate::stub(access_groups));
-		let user_b = room.register_member(MemberTemplate::stub(access_groups));
-
-		let object_id = room.test_create_object(user_a, access_groups, false).id.clone();
-		room.out_commands.clear();
-		let command = DeleteGameObjectCommand {
-			object_id: object_id.clone(),
-		};
-
-		assert!(matches!(
-			command.execute(&mut room, user_b),
-			Err(ServerCommandError::MemberNotOwnerGameObject {
-				object_id: _,
-				member_id: _
-			})
-		));
-		assert!(matches!(room.get_object_mut(&object_id), Ok(_)));
-		assert!(matches!(room.out_commands.pop_back(), None));
 	}
 }
