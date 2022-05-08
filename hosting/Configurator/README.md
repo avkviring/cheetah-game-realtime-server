ef7cb3748c26a3472ba11105b2241a783033d45d
Kviring Aleksey <alex@kviring.com>
2022-05-08 05:29:00 +0300

    feat(shared/0030-configuration-files.md): Переход на helm plugin.

--- hosting/Configurator/README.md
+++
@@ -1,3 +0,0 @@
-Утилита для создания ConfigurationMap
-
-[ADR](../../docs/adr/shared/0030-configuration-files.md)
--- hosting/Configurator/build-for-linux.sh
+++
@@ -1,1 +0,0 @@
-RUSTFLAGS="-C linker=x86_64-linux-musl-gcc -Ctarget-cpu=haswell -Ctarget-feature=+avx2" cargo build --release --target x86_64-unknown-linux-musl
