f3b3d525dd77b1a9a60f500624c06520e3b17e4d
Kviring Aleksey <alex@kviring.com>
2022-05-09 06:34:48 +0300

    refactor(client version): Восстановлен helm chart после рефакторинга.

--- hosting/charts/install-stage.sh
+++ hosting/charts/install-stage.sh
@@ -1,5 +1,6 @@
+set -e
 ## Выкладка платформы в kubernetes для локальных тестов
-cargo run --manifest-path ../Configurator/Cargo.toml ../example-config Platform/templates/
+helm cheetah-config-creator ../example-config Platform/templates/
 helm --kubeconfig /Users/kviring/Documents/.kube/stage.yaml -n $1 uninstall $1
 kubectl --kubeconfig /Users/kviring/Documents/.kube/stage.yaml delete --namespace $1 --all deployments,statefulsets,services,pods,pvc
 helm --kubeconfig /Users/kviring/Documents/.kube/stage.yaml -n $1 upgrade \
