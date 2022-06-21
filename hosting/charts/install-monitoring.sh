a9ef210b68ba8973f145857e6f12c006801c61db
Kviring Aleksey <alex@kviring.com>
2022-06-21 07:09:49 +0500

    chore(helm): Выделение values-shared для monitoring helm chart.

--- hosting/charts/install-monitoring.sh
+++ hosting/charts/install-monitoring.sh
@@ -1,6 +1,7 @@
 ## Обновление/инсталляция сервисов для мониторинга
-## параметр - production|dev, имя домена, пароль администратора для grafana, пароль для администрирования prometheus
+## параметр - production|stage, имя домена, пароль администратора для grafana, пароль для администрирования prometheus
 helm -n monitoring upgrade --install monitoring Monitoring \
+  -f Monitoring/values-shared.yaml \
   -f Monitoring/values-$1.yaml \
   --set global.prometheusAdminAuth=$(echo $4 | htpasswd -n -i admin | openssl enc -A -base64) \
   --set kube-prometheus-stack.prometheus.ingress.hosts[0]=prometheus.$2 \
