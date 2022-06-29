85773a14fb65bc86068786eb311c3ddf2000f8bc
Kviring Aleksey <alex@kviring.com>
2022-06-29 07:43:06 +0500

    feat(#858 - tonic error):  Функции для трассировки внутренних запросов.

--- server/libraries/Microservice/src/trace.rs
+++ server/libraries/Microservice/src/trace.rs
@@ -26,3 +26,34 @@ where
 	trace(error);
 	Status::unauthenticated("unauthenticated error")
 }
+
+///
+/// Использовать для  внутренних запросов, раскрываем информацию об ошибке клиенту
+///
+#[track_caller]
+pub fn trace_and_convert_to_tonic_internal_status_with_full_message<T>(error: T) -> Status
+where
+	T: Debug,
+{
+	Status::internal(trace(error))
+}
+
+///
+/// Использовать для  внутренних запросов, раскрываем информацию об ошибке клиенту
+///
+#[track_caller]
+pub fn trace_and_convert_to_tonic_unauthenticated_status_with_full_message<T>(error: T) -> Status
+where
+	T: Debug,
+{
+	Status::unauthenticated(trace(error))
+}
+
+fn trace<T>(error: T) -> String
+where
+	T: Debug,
+{
+	let msg = format!("{} {:?}", std::panic::Location::caller(), error);
+	error!("{}", msg);
+	msg
+}
