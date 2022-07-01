6cefb1b2ce80200c89f6f187bd9e94b9778a5f95
Kviring Aleksey <alex@kviring.com>
2022-07-01 07:34:55 +0500

    chore(ci): Текущего времени ожидания не хватает для развертывания тестового окружения.

--- hosting/charts/reinstall-stage.sh
+++ hosting/charts/reinstall-stage.sh
@@ -4,4 +4,10 @@
 helm cheetah-config-creator ../example-config Platform/templates/
 helm -n $1 uninstall $1
 kubectl delete --namespace $1 --all database,storage,deployments,statefulsets,services,pods,pvc
-helm -n $1 upgrade --install $1 Platform -f Platform/values-dev.yaml --set global.grpcDomain=$1.stage.cheetah.games --set global.platformImageVersion=$2 --wait-for-jobs --wait
+helm -n $1 upgrade --install $1 Platform \
+  -f Platform/values-dev.yaml \
+  --set global.grpcDomain=$1.stage.cheetah.games \
+  --set global.platformImageVersion=$2 \
+  --wait-for-jobs \
+  --wait \
+  --timeout 10m \
