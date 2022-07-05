ec191540ffc2183fe78e9ff1d2402c0612f29762
Даниил Делендик <daniil-delendik@yandex.ru>
2022-07-05 16:14:53 +0500

    refactor(ResultErrorTracer): ограничение типа Into<String> заменено на Display

--- server/libraries/Microservice/src/trace.rs
+++ server/libraries/Microservice/src/trace.rs
@@ -1,4 +1,4 @@
-use std::fmt::Debug;
+use std::fmt::{Debug, Display};
 
 use tracing::error;
 
@@ -6,7 +6,7 @@ pub trait ResultErrorTracer<T, E> {
 	fn trace_and_map_err<M, F, OutError>(self, details: M, f: F) -> Result<T, OutError>
 	where
 		F: FnOnce(E) -> OutError,
-		M: Into<String>;
+		M: Display;
 }
 
 impl<T, E> ResultErrorTracer<T, E> for Result<T, E>
@@ -16,12 +16,12 @@ where
 	fn trace_and_map_err<M, F, OutError>(self, details: M, f: F) -> Result<T, OutError>
 	where
 		F: FnOnce(E) -> OutError,
-		M: Into<String>,
+		M: Display,
 	{
 		match self {
 			Ok(v) => Ok(v),
 			Err(e) => {
-				let msg = format!("{} {:?}", details.into(), e);
+				let msg = format!("{} {:?}", details, e);
 				error!("{}", msg);
 				Err(f(e))
 			}
