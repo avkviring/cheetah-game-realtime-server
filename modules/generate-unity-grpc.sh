e4b2c252fb1045059d0bdafd43237e7b968bea60
Daniil Delendik <flaamjab@gmail.com>
2022-07-25 15:15:25 +0500

    refactor(generate-unity-grpc.sh): устранение дублирующегося кода

--- modules/generate-unity-grpc.sh
+++ modules/generate-unity-grpc.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
+
 project_dir=$(pwd)/../
 client_dir_from_protodir=../client
 
-generate_unity_grpc_files() {
-  suffix=$1
-  project_part=$2
-  for f in $(find . -type f -name \*$suffix); do
-    protodir=$(dirname $f)
-    protofile=$(basename $f)
-    ## matches.realtime.shared должен генерироваться в отдельный проект, а не в Runtime
-    if [[ "$protofile" == "matches.realtime.shared.proto" ]]; then
-        continue
-    fi
-    unity_project=games.cheetah.$(echo $protofile | sed "s/$suffix//")
-    grpc_out_path=$protodir/$client_dir_from_protodir/Unity/$unity_project/$project_part/GRPC
+run_protoc() {
+    protofile=$1
+    protodir=$2
+    grpc_out_path=$3
+
     mkdir -p $grpc_out_path
-    echo "$protodir ::: $protofile"
     docker run --rm -v$project_dir:/tmp/source -w /tmp/source/modules akviring/protoc:latest \
       protoc \
       --proto_path=$protodir \
@@ -24,19 +17,27 @@ generate_unity_grpc_files() {
       --csharp_out=$grpc_out_path \
       --experimental_allow_proto3_optional \
       $protofile
+}
+
+generate_unity_grpc_files() {
+  suffix=$1
+  project_part=$2
+  echo "Creating proto implementation files ($suffix)"
+  for f in $(find . -type f -name \*$suffix); do
+    protodir=$(dirname $f)
+    protofile=$(basename $f)
+    unity_project=games.cheetah.$(echo $protofile | sed "s/$suffix//")
+    if [[ $protofile == *.shared.proto ]]; then
+      unity_project+=".shared"
+    fi
+    grpc_out_path=$protodir/$client_dir_from_protodir/Unity/$unity_project/$project_part/GRPC
+    echo "  for $protofile in $(realpath --relative-to . $grpc_out_path)..."
+    run_protoc $protofile $protodir $grpc_out_path
   done
 }
 
 generate_unity_grpc_files .external.proto Runtime
+echo
 generate_unity_grpc_files .admin.proto Editor
+echo
 generate_unity_grpc_files .shared.proto Runtime
-
-## matches.realtime.shared должен генерироваться в отдельный проект, а не в Runtime
-docker run --rm -v$project_dir:/tmp/source -w /tmp/source/modules akviring/protoc:latest \
-  protoc \
-  --proto_path=/tmp/source/modules/matches/Realtime/proto/ \
-  --plugin=protoc-gen-grpc=/usr/bin/grpc_csharp_plugin \
-  --grpc_out=/tmp/source/modules/matches/Realtime/client/Unity/games.cheetah.matches.realtime.shared/Runtime/GRPC/ \
-  --csharp_out=/tmp/source/modules/matches/Realtime/client/Unity/games.cheetah.matches.realtime.shared/Runtime/GRPC/ \
-  --experimental_allow_proto3_optional \
-  matches.realtime.shared.proto
