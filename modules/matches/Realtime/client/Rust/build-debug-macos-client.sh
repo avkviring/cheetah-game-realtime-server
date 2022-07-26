14256c3683bf779e4b8fa38e4781823ef73b38a1
Kviring Aleksey <alex@kviring.com>
2022-07-26 07:54:32 +0500

    feat: Скрипты сборки тестовых клиентов realtime сервера

--- modules/matches/Realtime/client/Rust/build-debug-macos-client.sh
+++ modules/matches/Realtime/client/Rust/build-debug-macos-client.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
-## сборка relay клиента для тестирования на macos
+## сборка runtime клиента для тестирования на macos
 OUTPUT="../Unity/games.cheetah.matches.realtime/"
-
-## macos
 rm -f $OUTPUT/x86_64/libcheetah_matches_realtime_client.bundle
 cargo build
 cp ../../../../target/debug/libcheetah_matches_realtime_client.dylib "$OUTPUT/Runtime/Library/macos.bundle"
--- modules/matches/Realtime/client/Rust/build-ios-client.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-OUTPUT="../../../clients/Unity/Packages/games.cheetah.matches.relay/"
-cargo build --manifest-path Client/Cargo.toml --target aarch64-apple-ios --release
-
-cp ../../target/aarch64-apple-ios/release/libcheetah_matches_relay_client.a "$OUTPUT/iOS/libcheetah_matches_relay_client.a"
