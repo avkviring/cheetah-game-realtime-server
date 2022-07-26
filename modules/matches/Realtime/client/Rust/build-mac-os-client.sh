cbe25e06a4ee541a6765cb8485a1bf18bf6d1077
Kviring Aleksey <alex@kviring.com>
2022-07-26 07:53:19 +0500

    chore: Переход на новую версию Unity

--- modules/matches/Realtime/client/Rust/build-mac-os-client.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-## сборка relay клиента для тестирования на macos
-OUTPUT="../../../clients/Unity/Packages/games.cheetah.matches.relay/"
-## macos
-rm -f $OUTPUT/x86_64/libcheetah_matches_relay_client.bundle
-cargo build --manifest-path Client/Cargo.toml
-cp ../../target/debug/libcheetah_matches_relay_client.dylib "$OUTPUT/x86_64/libcheetah_matches_relay_client.bundle"
