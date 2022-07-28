6c78ed76db18251ff36126a3832dade918cf0b2a
Daniil Delendik <flaamjab@gmail.com>
2022-07-28 15:19:04 +0500

    ci(publish.unity.packages.yml): обновлены директории и наименования библиотечных файлов в пакете

--- modules/generate-unity-grpc-meta.sh
+++ modules/generate-unity-grpc-meta.sh
@@ -1,7 +1,10 @@
 generated_scripts=$(find -regex '.*/Unity/.*\(Editor\|Runtime\)/GRPC.*.cs')
+echo 'Creating Unity .meta files'
 for f in $generated_scripts; do
     project_part=$(basename $(dirname $(dirname $f)))
     file_name=$(basename $f)
+    
+    echo '  for $f...'
 
 echo "fileFormatVersion: 2
 guid: $(uuidgen --md5 -n @url -N Unity/$project_part/$file_name | tr -d '-')
