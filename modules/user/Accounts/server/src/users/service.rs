3cd0b8e57bb4697a36e374ee1fefc93108c3ad56
Kviring Aleksey <alex@kviring.com>
2022-08-08 10:46:46 +0500

    feat(postgresql-#95): Перевод users модуля user/Accounts на postgres.
    Signed-off-by: Kviring Aleksey <alex@kviring.com>

--- modules/user/Accounts/server/src/users/service.rs
+++ modules/user/Accounts/server/src/users/service.rs
@@ -1,37 +1,24 @@
+use sqlx::PgPool;
+
 use crate::users::user::User;
-use ydb::{TableClient, YdbOrCustomerError};
-use ydb_steroids::error::is_unique_violation_error;
-use ydb_steroids::{query, update};
 
 #[derive(Clone)]
 pub struct UserService {
-	ydb_table_client: TableClient,
+	pg_pool: PgPool,
 }
 
 impl UserService {
-	pub fn new(ydb_table_client: TableClient) -> Self {
-		Self { ydb_table_client }
+	pub fn new(pg_pool: PgPool) -> Self {
+		Self { pg_pool }
 	}
 
-	pub async fn create(&self) -> Result<User, YdbOrCustomerError> {
-		loop {
-			let user = User::default();
-			let result = update!(
-				self.ydb_table_client,
-				query!(
-					"insert into users (user,create_date) values($user,CurrentUtcDatetime())",
-					user=>user)
-			)
-			.await;
-			match result {
-				Ok(_) => return Ok(user),
-				Err(err) => {
-					if !is_unique_violation_error(&err) {
-						return Err(err);
-					}
-				}
-			}
-		}
+	pub async fn create(&self) -> Result<User, sqlx::Error> {
+		let user = User::default();
+		sqlx::query("insert into cheetah_user_account_users(user_uuid) values($1)")
+			.bind(user.0)
+			.execute(&self.pg_pool)
+			.await?;
+		Ok(user)
 	}
 }
 
@@ -39,50 +26,31 @@ impl UserService {
 pub mod tests {
 	use std::time::Duration;
 
+	use sqlx::postgres::PgRow;
+	use sqlx::Row;
 	use uuid::Uuid;
-	use ydb::Query;
 
-	use crate::ydb::test::setup_ydb;
+	use crate::postgres::test::setup_postgresql;
 
 	use super::UserService;
 
 	#[tokio::test]
 	pub async fn should_create() {
-		let (ydb_client, _instance) = setup_ydb().await;
-		let table_client = ydb_client.table_client();
-		let users = UserService::new(ydb_client.table_client());
+		let (pg_pool, _instance) = setup_postgresql().await;
+		let users = UserService::new(pg_pool.clone());
 
 		let user_a = users.create().await.unwrap();
 		let user_b = users.create().await.unwrap();
 
-		let users: Vec<_> = table_client
-			.retry_transaction(|mut t| async move {
-				let users = t.query(Query::new("select * from users")).await?;
-				Ok(users
-					.into_only_result()
-					.unwrap()
-					.rows()
-					.map(|mut row| {
-						let uuid: Option<ydb::Bytes> = row
-							.remove_field_by_name("user")
-							.unwrap()
-							.try_into()
-							.unwrap();
-						let uuid = Into::<Vec<u8>>::into(uuid.unwrap());
-						let uuid = Uuid::from_slice(uuid.as_slice()).unwrap();
-
-						let date: Option<Duration> = row
-							.remove_field_by_name("create_date")
-							.unwrap()
-							.try_into()
-							.unwrap();
-						(uuid, date.unwrap())
-					})
-					.collect())
-			})
+		let users: Vec<Uuid> = sqlx::query("select user_uuid from cheetah_user_account_users")
+			.fetch_all(&pg_pool)
 			.await
-			.unwrap();
-		assert!(users.iter().any(|i| i.0 == user_a.0));
-		assert!(users.iter().any(|i| i.0 == user_b.0));
+			.unwrap()
+			.iter_mut()
+			.map(|r| r.get(0))
+			.collect();
+
+		assert!(users.iter().any(|i| *i == user_a.0));
+		assert!(users.iter().any(|i| *i == user_b.0));
 	}
 }
--- modules/user/Accounts/server/src/users/user.rs
+++ modules/user/Accounts/server/src/users/user.rs
@@ -1,8 +1,6 @@
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-use ydb_steroids::converters::YDBValueConverter;
-
 #[derive(Debug, Serialize, Deserialize, Clone, Copy, Eq, PartialEq)]
 pub struct User(pub(crate) Uuid);
 
@@ -18,24 +16,8 @@ impl From<Uuid> for User {
 	}
 }
 
-impl From<ydb::Bytes> for User {
-	fn from(value: ydb::Bytes) -> Self {
-		let vec: Vec<_> = value.try_into().unwrap();
-		User(Uuid::from_slice(vec.as_slice()).unwrap())
-	}
-}
-
 impl From<u128> for User {
 	fn from(value: u128) -> Self {
 		Uuid::from_u128(value).into()
-	}
-}
-
-impl YDBValueConverter for User {
-	fn get_type_name(&self) -> &'static str {
-		"String"
-	}
-	fn to_ydb_value(&self) -> ydb::Value {
-		self.0.to_ydb_value()
 	}
 }
