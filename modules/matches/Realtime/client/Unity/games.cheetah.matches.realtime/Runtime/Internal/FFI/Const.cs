d28cd9032fd886808b80ceabae99c13cebda27ec
Daniil Delendik <flaamjab@gmail.com>
2022-08-08 15:08:46 +0500

    fix(realtime): отдавать приоритет редактору при выборе библиотечного файла

--- modules/matches/Realtime/client/Unity/games.cheetah.matches.realtime/Runtime/Internal/FFI/Const.cs
+++ modules/matches/Realtime/client/Unity/games.cheetah.matches.realtime/Runtime/Internal/FFI/Const.cs
@@ -2,20 +2,20 @@ namespace Cheetah.Matches.Realtime.Internal.FFI
 {
     internal static class Const
     {
-#if UNITY_ANDROID
+#if UNITY_EDITOR_WIN
+        public const string Library = "win";
+#elif UNITY_EDITOR_LINUX
+        public const string Library = "linux";            
+#elif UNITY_ANDROID
         public const string Library = "android-armv7";
+#elif UNITY_IOS
+        public const string Library = "__Internal";
 #elif UNITY_STANDALONE_WIN
         public const string Library = "win";
 #elif UNITY_STANDALONE_LINUX
         public const string Library = "linux";
 #elif UNITY_STANDALONE_OSX
         public const string Library = "macos";
-#elif UNITY_IOS
-        public const string Library = "__Internal";
-#elif UNITY_EDITOR_WIN
-        public const string Library = "win";
-#elif UNITY_EDITOR_LINUX
-        public const string Library = "linux";            
 #endif
 
         public const ushort MaxSizeStruct = 255;
