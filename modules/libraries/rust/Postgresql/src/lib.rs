1fd8ee187a527ffdcf3979dee4982b7442d83f22
Kviring Aleksey <alex@kviring.com>
2022-08-09 08:30:37 +0500

    feat(postgresql-#95): Отдельная библиотека для вспомогательных функций postgresql

---
+++ modules/libraries/rust/Postgresql/src/lib.rs
@@ -0,0 +1,33 @@
+use sqlx::postgres::PgPoolOptions;
+use sqlx::PgPool;
+
+pub async fn create_postgresql_pool_from_env() -> PgPool {
+	let db_port = cheetah_libraries_microservice::get_env("POSTGRES_PORT");
+	create_postgres_pool(
+		cheetah_libraries_microservice::get_env("POSTGRES_DB").as_str(),
+		cheetah_libraries_microservice::get_env("POSTGRES_USER").as_str(),
+		cheetah_libraries_microservice::get_env("POSTGRES_PASSWORD").as_str(),
+		cheetah_libraries_microservice::get_env("POSTGRES_HOST").as_str(),
+		db_port
+			.parse()
+			.unwrap_or_else(|_| panic!("POSTGRES_PORT is wrong {:?}", db_port)),
+	)
+	.await
+}
+
+pub async fn create_postgres_pool(
+	db: &str,
+	user: &str,
+	passwd: &str,
+	host: &str,
+	port: u16,
+) -> PgPool {
+	use std::time::Duration;
+	let uri = format!("postgres://{}:{}@{}:{}/{}", user, passwd, host, port, db);
+	PgPoolOptions::new()
+		.max_connections(5)
+		.acquire_timeout(Duration::from_secs(1))
+		.connect(&uri)
+		.await
+		.unwrap()
+}
--- modules/user/Accounts/server/src/bin/service.rs
+++ modules/user/Accounts/server/src/bin/service.rs
@@ -1,21 +1,12 @@
+use cheetah_libraries_postgresql::create_postgresql_pool_from_env;
 use cheetah_user_accounts::grpc::run_grpc_server;
-use cheetah_user_accounts::postgres::create_postgres_pool;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 	cheetah_libraries_microservice::init("accounts");
 
-	let db_port = cheetah_libraries_microservice::get_env("POSTGRES_PORT");
-	let pg_pool = create_postgres_pool(
-		cheetah_libraries_microservice::get_env("POSTGRES_DB").as_str(),
-		cheetah_libraries_microservice::get_env("POSTGRES_USER").as_str(),
-		cheetah_libraries_microservice::get_env("POSTGRES_PASSWORD").as_str(),
-		cheetah_libraries_microservice::get_env("POSTGRES_HOST").as_str(),
-		db_port
-			.parse()
-			.unwrap_or_else(|_| panic!("POSTGRES_PORT is wrong {:?}", db_port)),
-	)
-	.await;
+	let pg_pool = create_postgresql_pool_from_env().await;
+	sqlx::migrate!().run(&pg_pool).await.unwrap();
 
 	// ключи для генерации токенов
 	let jwt_public_key = cheetah_libraries_microservice::get_env("JWT_PUBLIC_KEY");
