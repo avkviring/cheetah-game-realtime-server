7c19554b532139a4ea77dfe211d27cde23f800f0
Kviring Aleksey <alex@kviring.com>
2022-08-09 06:39:45 +0500

    feat(postgresql-#95): Перевод token модуля user/Accounts на postgres.

--- modules/user/Accounts/server/migrations/0004_tokens.sql
+++ modules/user/Accounts/server/migrations/0004_tokens.sql
@@ -1,7 +1,8 @@
 create table cheetah_user_accounts_tokens
 (
-	user_uuid uuid primary key not null,
+	user_uuid uuid,
 	device    varchar(255),
 	token     uuid,
 	create_at timestamp default CURRENT_TIMESTAMP not null
-)
+);
+create unique index cheetah_user_accounts_tokens_index on cheetah_user_accounts_tokens (user_uuid, device, token);
