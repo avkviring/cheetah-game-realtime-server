dd083d8fd0743eeeb768c6e32d495c8ff64d67d1
Daniel Delendik <flaamjab@gmail.com>
2022-08-12 13:26:56 +0500

    fix(realtime): название DLL win -> windows

--- modules/matches/Realtime/client/Unity/games.cheetah.matches.realtime/Runtime/Internal/FFI/Const.cs
+++ modules/matches/Realtime/client/Unity/games.cheetah.matches.realtime/Runtime/Internal/FFI/Const.cs
@@ -3,7 +3,7 @@ namespace Cheetah.Matches.Realtime.Internal.FFI
     internal static class Const
     {
 #if UNITY_EDITOR_WIN
-        public const string Library = "win";
+        public const string Library = "windows";
 #elif UNITY_EDITOR_LINUX
         public const string Library = "linux";            
 #elif UNITY_ANDROID
@@ -11,7 +11,7 @@ namespace Cheetah.Matches.Realtime.Internal.FFI
 #elif UNITY_IOS
         public const string Library = "__Internal";
 #elif UNITY_STANDALONE_WIN
-        public const string Library = "win";
+        public const string Library = "windows";
 #elif UNITY_STANDALONE_LINUX
         public const string Library = "linux";
 #elif UNITY_STANDALONE_OSX
