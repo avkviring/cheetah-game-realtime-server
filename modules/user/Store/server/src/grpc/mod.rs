c207a0c5903cbe56ba3bff7c0e238d8924c5af29
Daniil Delendik <flaamjab@gmail.com>
2022-08-15 15:29:27 +0500

    refactor(userstore): выделить код авторизации в библиотеку

--- modules/user/Store/server/src/grpc/mod.rs
+++ modules/user/Store/server/src/grpc/mod.rs
@@ -1,17 +1,3 @@
-use std::{error::Error, net::SocketAddr};
-
-use jwt_tonic_user_uuid::JWTUserTokenParser;
-use sqlx::PgPool;
-use tonic::{transport::Server, Request, Status};
-use tonic_health::ServingStatus;
-use uuid::Uuid;
-
-use cheetah_libraries_microservice::trace::trace_err;
-use update::UpdateService;
-use userstore::update_server::UpdateServer;
-
-use self::{fetch::FetchService, userstore::fetch_server::FetchServer};
-
 mod fetch;
 mod reply;
 mod update;
@@ -19,6 +5,17 @@ mod value;
 mod userstore {
 	tonic::include_proto!("cheetah.user.store.external");
 }
+
+use std::{error::Error, net::SocketAddr};
+
+use cheetah_libraries_microservice::auth::JwtAuthInterceptor;
+use sqlx::PgPool;
+use tonic::transport::Server;
+use tonic_health::ServingStatus;
+
+use fetch::FetchService;
+use update::UpdateService;
+use userstore::{fetch_server::FetchServer, update_server::UpdateServer};
 
 pub struct Service {
 	pg_pool: PgPool,
@@ -36,7 +33,7 @@ impl Service {
 	pub async fn serve(&self, addr: SocketAddr) -> Result<(), Box<dyn Error>> {
 		let (mut health_reporter, health_service) = tonic_health::server::health_reporter();
 
-		let update_service = UpdateService::new(self.pg_pool.clone(), self.jwt_public_key.clone());
+		let update_service = UpdateService::new(self.pg_pool.clone());
 
 		health_reporter
 			.set_service_status("", ServingStatus::Serving)
@@ -46,33 +43,24 @@ impl Service {
 			.set_serving::<UpdateServer<UpdateService>>()
 			.await;
 
-		let fetch_service = FetchService::new(self.pg_pool.clone(), self.jwt_public_key.clone());
+		let fetch_service = FetchService::new(self.pg_pool.clone());
 		health_reporter
 			.set_serving::<FetchServer<FetchService>>()
 			.await;
 
+		let auth_interceptor = JwtAuthInterceptor::new(self.jwt_public_key.to_owned());
+		let update_server =
+			UpdateServer::with_interceptor(update_service, auth_interceptor.clone());
+		let fetch_server = FetchServer::with_interceptor(fetch_service, auth_interceptor.clone());
+
 		Server::builder()
 			.accept_http1(true)
 			.add_service(tonic_web::enable(health_service))
-			.add_service(tonic_web::enable(UpdateServer::new(update_service)))
-			.add_service(tonic_web::enable(FetchServer::new(fetch_service)))
+			.add_service(tonic_web::enable(update_server))
+			.add_service(tonic_web::enable(fetch_server))
 			.serve(addr)
 			.await?;
 
 		Ok(())
-	}
-}
-
-fn verify_credentials<T>(request: Request<T>, jwt_public_key: &str) -> Result<(Uuid, T), Status> {
-	let parser = JWTUserTokenParser::new(jwt_public_key.to_string());
-	match parser.get_user_uuid_from_grpc(request.metadata()) {
-		Err(e) => {
-			trace_err("Unauthorized access attempt", e);
-			Err(Status::permission_denied(""))
-		}
-		Ok(user) => {
-			let args = request.into_inner();
-			Ok((user, args))
-		}
 	}
 }
