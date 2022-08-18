dc859bfccc0b486204121360ccdf13d2e51daa2b
Kviring Aleksey <alex@kviring.com>
2022-08-18 07:53:24 +0500

    feat(#143): Настройка для отключения удаления контейнеров при неудачном запуске локального кластера.

--- modules/Platform/client/Unity/games.cheetah.platform/Editor/LocalServer/SystemConfig/SystemApplicationsConfigurator.cs
+++ modules/Platform/client/Unity/games.cheetah.platform/Editor/LocalServer/SystemConfig/SystemApplicationsConfigurator.cs
@@ -1,41 +1,41 @@
+using Cheetah.Platform.Editor.LocalServer.Applications;
 using Cheetah.Platform.Editor.LocalServer.Docker;
 using Cheetah.Platform.Editor.UIElements;
 using UnityEditor;
 using UnityEngine.UIElements;
 
-namespace Cheetah.Platform.Editor.LocalServer.GrpcProxy
+namespace Cheetah.Platform.Editor.LocalServer.SharedConfig
 {
     /// <summary>
     /// UI для настройки общих параметров микросервисов
     /// - хост/порт для grpc/http прокси
     /// - включение INFO логов для сервисов
     /// </summary>
-    public class GrpcProxyApplicationConfigurator : IApplicationsConfigurator, GrpcProxyApplication.IConfig
+    public class SystemApplicationsConfigurator : IApplicationsConfigurator, GrpcProxyApplication.IConfig
     {
         private const string ShowInfoLogsPrefsKey = "cheetah.platform.microservice.show_info_logs";
-        public string Title => "GRPC Services";
+        public string Title => "System settings";
         public string Host => address?.Host ?? "127.0.0.1";
         public int Port => address?.Port ?? 7777;
 
+        public bool KeepFailedContainers => keepFailedContainersUI.value;
+        public bool ShowInfoLogs => showInfoLogsUI.value;
+
         private NetworkAddressElement address;
+        private Toggle keepFailedContainersUI;
+        private Toggle showInfoLogsUI;
 
 
         public VisualElement CreateUI()
         {
             VisualTreeAsset uiAsset =
-                AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Packages/games.cheetah.platform/Editor/LocalServer/GrpcProxy/UI.uxml");
+                AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Packages/games.cheetah.platform/Editor/LocalServer/SystemConfig/UI.uxml");
             var ui = uiAsset.Instantiate();
-
-            var showInfoLogs = ui.Q<Toggle>("show_info_logs");
+            showInfoLogsUI = ui.Q<Toggle>("show_info_logs");
             var showInfoLogValue = EditorPrefs.GetBool(ShowInfoLogsPrefsKey, false);
-            DockerLogWatcher.ShowInfoLogs = showInfoLogValue;
-            showInfoLogs.value = showInfoLogValue;
-            showInfoLogs.RegisterCallback<ChangeEvent<bool>>(e =>
-            {
-                EditorPrefs.SetBool(ShowInfoLogsPrefsKey, e.newValue);
-                DockerLogWatcher.ShowInfoLogs = e.newValue;
-            });
-
+            showInfoLogsUI.value = showInfoLogValue;
+            showInfoLogsUI.RegisterCallback<ChangeEvent<bool>>(e => { EditorPrefs.SetBool(ShowInfoLogsPrefsKey, e.newValue); });
+            keepFailedContainersUI = ui.Q<Toggle>("keep_failed_containers");
             address = ui.Q<NetworkAddressElement>("address");
 
             return ui;
