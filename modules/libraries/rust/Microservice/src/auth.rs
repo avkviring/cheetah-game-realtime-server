d5030f06becc09ea859a5d958503dc2a2dede64b
Daniil Delendik <flaamjab@gmail.com>
2022-08-19 16:11:53 +0500

    test(libraries/rust/Microservice): добавлены дополнительные тесты

--- modules/libraries/rust/Microservice/src/auth.rs
+++ modules/libraries/rust/Microservice/src/auth.rs
@@ -8,7 +8,7 @@ use tonic::{
 };
 use uuid::Uuid;
 
-use crate::trace::trace_err;
+use crate::trace;
 
 pub const USER_KEY: &str = "user";
 
@@ -28,7 +28,7 @@ impl Interceptor for JwtAuthInterceptor {
 		let parser = JWTUserTokenParser::new(self.jwt_public_key.clone());
 		match parser.get_user_uuid_from_grpc(request.metadata()) {
 			Err(e) => {
-				trace_err("Unauthorized access attempt", e);
+				trace::err("Unauthorized access attempt", e);
 				Err(Status::permission_denied(""))
 			}
 			Ok(uuid) => {
@@ -42,7 +42,6 @@ impl Interceptor for JwtAuthInterceptor {
 
 fn store_user_uuid(metadata: &mut MetadataMap, uuid: &Uuid) {
 	let uuid_str = uuid.as_simple().to_string();
-	eprintln!("{}", uuid_str);
 	metadata.insert(USER_KEY, AsciiMetadataValue::from_str(&uuid_str).unwrap());
 }
 
@@ -54,10 +53,12 @@ pub fn load_user_uuid(metadata: &MetadataMap) -> Uuid {
 
 #[cfg(test)]
 mod test {
-	use tonic::metadata::{AsciiMetadataValue, BinaryMetadataValue};
+	use tonic::metadata::MetadataValue;
 
-	use super::Uuid;
-	use super::{store_user_uuid, MetadataMap};
+	use super::{
+		store_user_uuid, AsciiMetadataValue, Interceptor, JwtAuthInterceptor, MetadataMap, Request,
+		Uuid,
+	};
 
 	#[test]
 	fn test_store_user_uuid() {
@@ -78,4 +79,39 @@ mod test {
 		let mut md = MetadataMap::new();
 		md.append("yes-bib", AsciiMetadataValue::from_static("fail"));
 	}
+
+	#[test]
+	#[should_panic]
+	fn test_interceptor_unauthorized_request() {
+		let mut interceptor = JwtAuthInterceptor::new("lol".into());
+		let request = Request::new(());
+		interceptor.call(request).expect("Bad request metadata");
+	}
+
+	#[test]
+	fn test_interceptor_happy_request() {
+		let mut request = Request::new(());
+		let md = request.metadata_mut();
+		let auth_value = MetadataValue::from_str(&format!("Bear {}", JWT_TOKEN)).unwrap();
+		md.insert(AUTH_METADATA_KEY, auth_value);
+		let mut interceptor = JwtAuthInterceptor::new(JWT_PUBLIC_KEY.into());
+		interceptor
+			.call(request)
+			.expect("Happy request failed authorization");
+	}
+
+	const AUTH_METADATA_KEY: &str = "authorization";
+
+	const JWT_TOKEN: &str = "eyJhbGciOiJFUzI1NiJ9.eyJuYW1lIjoiQXBwbGUifQ.sTBwTCkNT3eBeFi6vuxZVIHBFTOqdUeodl-xKPitRMXeIcqe31CT-cQ1m2WFLGGeCLRfsjZSsdbELU-8gXSBJg";
+
+	const JWT_PUBLIC_KEY: &str = "----BEGIN PUBLIC KEY-----
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEVVHNXKxoUNkoX9hnOJpSz6K2KDfi
+gxaSXu+FIpP32qvcDgZPZ01tjnGjOysyPxRoZaMu/d9rHi3ulbceoYwS+Q==
+-----END PUBLIC KEY-----";
+
+	const JWT_PRIVATE_KEY: &str = "-----BEGIN PRIVATE KEY-----
+MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgcg7dsJWSz8f40cEv
+BTeGSzANXGlEzutd9IIm6/inl0ahRANCAARVUc1crGhQ2Shf2Gc4mlLPorYoN+KD
+FpJe74Uik/faq9wOBk9nTW2OcaM7KzI/FGhloy7932seLe6Vtx6hjBL5
+-----END PRIVATE KEY-----";
 }
