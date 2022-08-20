4f7a738a77a283bfe26e3f35f9a0fe42ba3c86c2
Daniil Delendik <flaamjab@gmail.com>
2022-08-20 07:59:12 +0500

    test(lib/rust/microservice/auth): исправлены и дополнены тесты

--- modules/libraries/rust/Microservice/src/auth.rs
+++ modules/libraries/rust/Microservice/src/auth.rs
@@ -53,7 +53,7 @@ pub fn load_user_uuid(metadata: &MetadataMap) -> Uuid {
 
 #[cfg(test)]
 mod test {
-	use tonic::metadata::MetadataValue;
+	use tonic::Code;
 
 	use super::{
 		store_user_uuid, AsciiMetadataValue, Interceptor, JwtAuthInterceptor, MetadataMap, Request,
@@ -61,7 +61,7 @@ mod test {
 	};
 
 	#[test]
-	fn test_store_user_uuid() {
+	fn test_store_user_uuid_does_not_panic() {
 		let mut md = MetadataMap::new();
 		let uuid = Uuid::new_v4();
 		store_user_uuid(&mut md, &uuid)
@@ -81,37 +81,61 @@ mod test {
 	}
 
 	#[test]
-	#[should_panic]
 	fn test_interceptor_unauthorized_request() {
-		let mut interceptor = JwtAuthInterceptor::new("lol".into());
+		let mut interceptor = JwtAuthInterceptor::new("broken key".into());
 		let request = Request::new(());
-		interceptor.call(request).expect("Bad request metadata");
+		let result = interceptor.call(request);
+
+		match result {
+			Err(s) => assert!(s.code() == Code::PermissionDenied),
+			Ok(_) => panic!("An unauthorized request slipped through interceptor"),
+		}
 	}
 
 	#[test]
 	fn test_interceptor_happy_request() {
-		let mut request = Request::new(());
-		let md = request.metadata_mut();
-		let auth_value = MetadataValue::from_str(&format!("Bear {}", JWT_TOKEN)).unwrap();
-		md.insert(AUTH_METADATA_KEY, auth_value);
+		let request = happy_request();
 		let mut interceptor = JwtAuthInterceptor::new(JWT_PUBLIC_KEY.into());
 		interceptor
 			.call(request)
 			.expect("Happy request failed authorization");
 	}
 
+	#[test]
+	fn test_interceptor_request_with_bad_user_is_corrected() {
+		let mut request = happy_request();
+		let md = request.metadata_mut();
+		md.insert("user", "penguins".parse().unwrap());
+		let mut interceptor = JwtAuthInterceptor::new(JWT_PUBLIC_KEY.into());
+
+		let modified_request = interceptor
+			.call(request)
+			.expect("Interceptor failed to process happy request");
+		let md = modified_request.metadata();
+
+		let uuid = md.get("user").unwrap();
+		assert!(uuid == JWT_TOKEN_EXTRACTED_UUID);
+	}
+
+	fn happy_request() -> Request<()> {
+		let mut request = Request::new(());
+		let md = request.metadata_mut();
+		let auth_value = format!("JWT {}", JWT_TOKEN).parse().unwrap();
+		md.insert(AUTH_METADATA_KEY, auth_value);
+
+		request
+	}
+
+	const JWT_TOKEN_EXTRACTED_UUID: &str = "deadbeefbeefbeefbeefdeadbeef6666";
+
 	const AUTH_METADATA_KEY: &str = "authorization";
 
-	const JWT_TOKEN: &str = "eyJhbGciOiJFUzI1NiJ9.eyJuYW1lIjoiQXBwbGUifQ.sTBwTCkNT3eBeFi6vuxZVIHBFTOqdUeodl-xKPitRMXeIcqe31CT-cQ1m2WFLGGeCLRfsjZSsdbELU-8gXSBJg";
+	const JWT_TOKEN: &str = "eyJleHAiOjk5OTk5OTk5OTk5LCJ1c2VyIjoiZGVhZGJlZWYtYm\
+VlZi1iZWVmLWJlZWYtZGVhZGJlZWY2NjY2In0.5TM5QO1qYU5Xe2RkwC_V_pr7UXTgr7BCkKrKUByfM\
+-EvRd1bbwlxydyhPikeQ5iNwPG_d6XmqiSqw-xt3YG_9Q";
 
-	const JWT_PUBLIC_KEY: &str = "----BEGIN PUBLIC KEY-----
+	const JWT_PUBLIC_KEY: &str = "-----BEGIN PUBLIC KEY-----
 MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEVVHNXKxoUNkoX9hnOJpSz6K2KDfi
 gxaSXu+FIpP32qvcDgZPZ01tjnGjOysyPxRoZaMu/d9rHi3ulbceoYwS+Q==
 -----END PUBLIC KEY-----";
-
-	const JWT_PRIVATE_KEY: &str = "-----BEGIN PRIVATE KEY-----
-MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgcg7dsJWSz8f40cEv
-BTeGSzANXGlEzutd9IIm6/inl0ahRANCAARVUc1crGhQ2Shf2Gc4mlLPorYoN+KD
-FpJe74Uik/faq9wOBk9nTW2OcaM7KzI/FGhloy7932seLe6Vtx6hjBL5
------END PRIVATE KEY-----";
 }
