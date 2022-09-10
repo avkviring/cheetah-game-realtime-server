75a907b2e87b10d0f9fce7dbc890f5c67fe8f43f
Kviring Aleksey <alex@kviring.com>
2022-09-10 13:21:42 +0500

    refactor(#131 matches/realtime/embedded): Класс для создания и управления embedded сервером.

---
+++ modules/matches/Realtime/embedded/src/lib.rs
@@ -0,0 +1,148 @@
+use std::net::SocketAddr;
+use std::sync::Arc;
+use std::time::Duration;
+
+use tokio::runtime::Runtime;
+use tokio::sync::Mutex;
+
+use cheetah_matches_realtime::room::template::config::{GameObjectTemplate, MemberTemplate, RoomTemplate};
+use cheetah_matches_realtime::server::manager::RoomsServerManager;
+use cheetah_matches_realtime::ServerBuilder;
+use cheetah_matches_realtime_common::room::access::AccessGroups;
+use cheetah_matches_realtime_common::room::{MemberPrivateKey, RoomId, RoomMemberId};
+
+///
+/// Обертка для запуска сервера из so/dll.
+/// - методы не могут быть async так как они будут вызываться как методы so/dll
+///
+pub struct EmbeddedServerWrapper {
+	runtime: Runtime,
+	pub manager: Arc<Mutex<RoomsServerManager>>,
+	pub game_socket_addr: SocketAddr,
+	pub internal_grpc_socket_addr: SocketAddr,
+	pub admin_grpc_socket_addr: SocketAddr,
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum EmbeddedServerWrapperError {
+	#[error("GrpcServicesNotStarted")]
+	GrpcServicesNotStarted,
+}
+
+impl EmbeddedServerWrapper {
+	pub fn run_new_server() -> anyhow::Result<Self> {
+		let runtime = tokio::runtime::Builder::new_multi_thread().worker_threads(2).enable_io().build().unwrap();
+		let server = runtime.block_on(async move { ServerBuilder::default().build().await });
+		let manager = server.manager.clone();
+		let game_socket_addr = server.game_socket_addr.clone();
+		let internal_grpc_socket_addr = server.internal_grpc_tcp_listener.local_addr()?;
+		let admin_grpc_socket_addr = server.admin_grpc_tcp_listener.local_addr()?;
+		runtime.spawn(async move {
+			server.run().await;
+		});
+
+		Self::wait_open_grpc_ports(internal_grpc_socket_addr, admin_grpc_socket_addr)?;
+
+		Ok(EmbeddedServerWrapper {
+			runtime,
+			game_socket_addr,
+			internal_grpc_socket_addr,
+			admin_grpc_socket_addr,
+			manager,
+		})
+	}
+
+	fn wait_open_grpc_ports(internal_grpc_socket_addr: SocketAddr, admin_grpc_socket_addr: SocketAddr) -> Result<(), EmbeddedServerWrapperError> {
+		let mut counter = 0;
+		while !port_scanner::scan_port_addr(internal_grpc_socket_addr) || !port_scanner::scan_port_addr(admin_grpc_socket_addr) {
+			std::thread::sleep(Duration::from_millis(10));
+			counter += 1;
+			if counter > 100 {
+				return Err(EmbeddedServerWrapperError::GrpcServicesNotStarted);
+			}
+		}
+		Ok(())
+	}
+
+	pub fn create_room(&self, room_template: RoomTemplate) -> anyhow::Result<RoomId> {
+		let manager = self.manager.clone();
+		Ok(self.runtime.block_on(async move { manager.lock().await.create_room(room_template) })?)
+	}
+
+	pub fn create_member(
+		&self,
+		room_id: RoomId,
+		groups: AccessGroups,
+		objects: Vec<GameObjectTemplate>,
+	) -> anyhow::Result<(RoomMemberId, MemberPrivateKey)> {
+		let manager = self.manager.clone();
+		let member_template = MemberTemplate::new_member(groups, objects);
+		let key = member_template.private_key.clone();
+		Ok((
+			self.runtime
+				.block_on(async move { manager.lock().await.create_member(room_id, member_template) })?,
+			key,
+		))
+	}
+
+	pub fn shutdown(self) {
+		let manager = self.manager.clone();
+		self.runtime.block_on(async move { manager.lock().await.shutdown() });
+		self.runtime.shutdown_background();
+	}
+}
+
+#[cfg(test)]
+mod test {
+	use std::time::Duration;
+
+	use cheetah_matches_realtime::room::template::config::RoomTemplate;
+	use cheetah_matches_realtime_common::room::access::AccessGroups;
+
+	use crate::EmbeddedServerWrapper;
+
+	#[test]
+	fn should_open_tcp_ports_after_start() {
+		let server = EmbeddedServerWrapper::run_new_server().unwrap();
+		let admin_grpc_port = server.admin_grpc_socket_addr.port();
+		let internal_grpc_port = server.internal_grpc_socket_addr.port();
+		assert!(port_scanner::scan_port(admin_grpc_port));
+		assert!(port_scanner::scan_port(internal_grpc_port));
+	}
+
+	#[test]
+	fn should_use_different_port_for_different_server() {
+		let server_a = EmbeddedServerWrapper::run_new_server().unwrap();
+		let server_b = EmbeddedServerWrapper::run_new_server().unwrap();
+		assert_ne!(server_a.game_socket_addr, server_b.game_socket_addr);
+		assert_ne!(server_a.admin_grpc_socket_addr, server_b.admin_grpc_socket_addr);
+		assert_ne!(server_a.internal_grpc_socket_addr, server_b.internal_grpc_socket_addr);
+	}
+
+	#[test]
+	fn should_create_room() {
+		let server = EmbeddedServerWrapper::run_new_server().unwrap();
+		let room_id = server.create_room(RoomTemplate::default()).unwrap();
+		let dump = server.runtime.block_on(async { server.manager.lock().await.dump(room_id) });
+		assert!(dump.is_ok())
+	}
+
+	#[test]
+	fn should_create_member() {
+		let server = EmbeddedServerWrapper::run_new_server().unwrap();
+		let room_id = server.create_room(RoomTemplate::default()).unwrap();
+		let (member_id, _member_private_key) = server.create_member(room_id, AccessGroups(8), Vec::new()).unwrap();
+		assert!(member_id > 0);
+	}
+
+	#[test]
+	fn should_shutdown_server() {
+		let server = EmbeddedServerWrapper::run_new_server().unwrap();
+		let admin_grpc_port = server.admin_grpc_socket_addr.port();
+		let internal_grpc_port = server.internal_grpc_socket_addr.port();
+		server.shutdown();
+		std::thread::sleep(Duration::from_millis(100));
+		assert!(!port_scanner::scan_port(admin_grpc_port));
+		assert!(!port_scanner::scan_port(internal_grpc_port));
+	}
+}
