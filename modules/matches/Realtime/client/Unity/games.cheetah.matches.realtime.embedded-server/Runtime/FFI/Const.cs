f9984a5077d3873e3135d7784049049df0faadac
Kviring Aleksey <alex@kviring.com>
2022-09-19 11:15:38 +0500

    feat(#131 realtime-embedded-server): Настройка сборки на CI

--- modules/matches/Realtime/client/Unity/games.cheetah.matches.realtime.embedded-server/Runtime/FFI/Const.cs
+++ modules/matches/Realtime/client/Unity/games.cheetah.matches.realtime.embedded-server/Runtime/FFI/Const.cs
@@ -2,6 +2,14 @@ namespace Cheetah.Matches.Realtime.EmbeddedServer.FFI
 {
     internal static class Const
     {
-        public const string Library = "embedded_realtime_server";
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+        public const string Library = "cheetah_matches_realtime_embedded";
+#elif UNITY_EDITOR_LINUX || UNITY_STANDALONE_LINUX
+        public const string Library = "libcheetah_matches_realtime_embedded";
+#elif UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+        public const string Library = "libcheetah_matches_realtime_embedded";
+#else
+    public const string Library = "not-supported";
+#endif
     }
 }
