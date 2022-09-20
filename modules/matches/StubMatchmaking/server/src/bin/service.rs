ba2b23b5d46655efb4bb557849aa159db3391a35
Daniil Delendik <flaamjab@gmail.com>
2022-09-20 15:11:55 +0500

    feat(#185): реализован подбор комнаты в соответствии с правилами матчмейкинга в StubMatchmaking

--- modules/matches/StubMatchmaking/server/src/bin/service.rs
+++ modules/matches/StubMatchmaking/server/src/bin/service.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use tonic::transport::Server;
 
 use cheetah_matches_stub_matchmaking::proto::matches::matchmaking;
@@ -8,7 +10,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 	cheetah_libraries_microservice::init("matches.stubmatchmaking");
 	let factory_url = cheetah_libraries_microservice::get_internal_srv_uri_from_env("CHEETAH_MATCHES_FACTORY");
 	let jwt_public_key = cheetah_libraries_microservice::get_env("JWT_PUBLIC_KEY");
-	let service = StubMatchmakingService::new(factory_url, jwt_public_key);
+	// TODO: add initialization from config
+	let service = StubMatchmakingService::new(factory_url, jwt_public_key, HashMap::new());
 	let grpc_service = matchmaking::external::matchmaking_server::MatchmakingServer::new(service);
 	Server::builder()
 		.accept_http1(true)
