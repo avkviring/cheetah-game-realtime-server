5d0b36773d7278884a73e0d8d029350295dc2f42
Kviring Aleksey <alex@kviring.com>
2022-10-30 08:14:59 +0300

    refactor(Realtime): Настройка CI для новых каталогов с клиентскими проектами

--- modules/matches/Realtime/client/rust/Embedded/build-debug-linux-client.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-## сборка runtime клиента для тестирования на macos
-OUTPUT="../client/Unity/games.cheetah.matches.realtime.embedded-server/"
-cd ../../../
-cross build --manifest-path matches/Realtime/Embedded/Cargo.toml --target x86_64-unknown-linux-gnu
-#cp ../../../target/debug/libcheetah_matches_realtime_embedded.dylib
-# "$OUTPUT/Runtime/FFI/Library/embedded_realtime_server.bundle"
--- modules/matches/Realtime/client/rust/Embedded/build-debug-macos-client.sh
+++ modules/matches/Realtime/client/rust/Embedded/build-debug-macos-client.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 ## сборка runtime клиента для тестирования на macos
-OUTPUT="../client/Unity/games.cheetah.matches.realtime.embedded-server/"
+OUTPUT="../../Unity/games.cheetah.matches.realtime.embedded-server/"
 cargo build
-
-cp ../../../target/debug/libcheetah_matches_realtime_embedded.dylib "$OUTPUT/Runtime/FFI/Library/libcheetah_matches_realtime_embedded.bundle"
+cp ../../../../../target/debug/libcheetah_matches_realtime_embedded.dylib "$OUTPUT/Runtime/FFI/Library/libcheetah_matches_realtime_embedded.bundle"
