41f609f667f018a28f92168b200f9ceb04837f3f
Kviring Aleksey <alex@kviring.com>
2022-11-15 12:31:29 +0300

    feat(#248): Поддержка релиза nuget пакетов

---
+++ scripts/release/nuget-packages.sh
@@ -0,0 +1,17 @@
+set -e
+project_dir=$(pwd)
+function push() {
+  project_file=$1
+  sed -i.back "s/999.999.999/$VERSION/g" $project_file
+  dir=$(dirname $project_file)
+  cd $dir
+  dotnet pack --configuration Release
+  nupkg_file=$(ls bin/Release/*.nupkg)
+  dotnet nuget push $nupkg_file -s https://api.nuget.org/v3/index.json -k $NUGET_PUSH_KEY
+  cd $project_dir
+  mv $project_file.back $project_file
+}
+
+push modules/matches/Realtime/client/Net/Embedded/Src/Src.csproj
+push modules/matches/Registry/proto/RegistryProto.csproj
+push modules/matches/Realtime/proto/RealtimeProto.csproj
