7bcee156bbf9ab2b1281b7bcf94e907fe43006d3
Kviring Aleksey <alex@kviring.com>
2022-12-02 08:20:14 +0500

    feat: Упрощены скрипты генерации grpc файлов для Unity.

--- scripts/generate-all.sh
+++ scripts/generate-all.sh
@@ -3,7 +3,14 @@ set -e
 cd ../
 
 echo "=== Generate Unity GRPC "
-scripts/generators/grpc/unity.sh
+source scripts/generators/unity.sh
 
-echo "=== Generate Csharp GRPC "
-scripts/generators/grpc/csharp.sh
+generate_unity_grpc_files modules/matches/Realtime/proto/matches.realtime.admin.proto modules/matches/Realtime/client/Unity/games.cheetah.matches.realtime/Editor/GRPC
+generate_unity_grpc_files modules/matches/Realtime/proto/matches.realtime.shared.proto modules/matches/Realtime/client/Unity/games.cheetah.matches.realtime.shared/Runtime/GRPC
+generate_unity_grpc_files modules/matches/Realtime/proto/matches.realtime.internal.proto modules/matches/Realtime/client/Unity/games.cheetah.matches.realtime.grpc.internal/Runtime/GRPC
+
+# мета файлы создаем только для linux - так как
+# именно он используется для вывода релиза
+if [[ ($OSTYPE == 'linux'*) ]]; then
+  generate_meta_files
+fi
