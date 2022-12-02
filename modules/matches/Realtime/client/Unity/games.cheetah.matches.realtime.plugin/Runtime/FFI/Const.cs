5855a1ac5303009c72b15843dc75c1e81026d507
Kviring Aleksey <alex@kviring.com>
2022-12-02 16:24:03 +0500

    feat: Добавлен проект games.cheetah.matches.realtime.plugin

---
+++ modules/matches/Realtime/client/Unity/games.cheetah.matches.realtime.plugin/Runtime/FFI/Const.cs
@@ -0,0 +1,15 @@
+namespace Cheetah.Matches.Realtime.Plugin.FFI
+{
+    internal static class Const
+    {
+#if UNITY_64
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+        public const string Library = "cheetah_matches_realtime_server_plugin";
+#else
+        public const string Library = "libcheetah_matches_realtime_server_plugin";
+#endif
+#else
+        public const string Library = "cheetah_matches_realtime_server_plugin";
+#endif
+    }
+}
