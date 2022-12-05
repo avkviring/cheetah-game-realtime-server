01631f74284bdc8c897e0ff050ddf6cddd6e1ae9
Kviring Aleksey <alex@kviring.com>
2022-12-05 15:42:37 +0500

    chore: Настройка ci на новые пути проектов.

--- scripts/generate-all.sh
+++ scripts/generate-all.sh
@@ -5,9 +5,9 @@ cd ../
 echo "=== Generate Unity GRPC "
 source scripts/generators/unity.sh
 
-generate_unity_grpc_files modules/matches/Realtime/proto/matches.realtime.admin.proto modules/matches/Realtime/client/Unity/games.cheetah.matches.realtime.grpc.admin/Editor/
-generate_unity_grpc_files modules/matches/Realtime/proto/matches.realtime.shared.proto modules/matches/Realtime/client/Unity/games.cheetah.matches.realtime.grpc.shared/Runtime/
-generate_unity_grpc_files modules/matches/Realtime/proto/matches.realtime.internal.proto modules/matches/Realtime/client/Unity/games.cheetah.matches.realtime.grpc.internal/Runtime/
+generate_unity_grpc_files modules/matches/Realtime/proto/matches.realtime.admin.proto client/Unity/Packages/games.cheetah.matches.realtime.grpc.admin/Editor/
+generate_unity_grpc_files modules/matches/Realtime/proto/matches.realtime.shared.proto client/Unity/Packages/games.cheetah.matches.realtime.grpc.shared/Runtime/
+generate_unity_grpc_files modules/matches/Realtime/proto/matches.realtime.internal.proto client/Unity/Packages/games.cheetah.matches.realtime.grpc.internal/Runtime/
 
 # мета файлы создаем только для linux - так как
 # именно он используется для вывода релиза
