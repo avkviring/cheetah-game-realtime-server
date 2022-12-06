81cc6c8339698476208058a51ecf41722bfd256d
Kviring Aleksey <alex@kviring.com>
2022-12-06 15:38:23 +0500

    feat: protoc_src не поддерживает windows.

--- rust/Plugin/build.rs
+++ rust/Plugin/build.rs
@@ -1,7 +1,13 @@
 use std::io::Error;
+use std::path::PathBuf;
 
 fn main() -> Result<(), Error> {
-	std::env::set_var("PROTOC", protobuf_src::protoc());
+	#[cfg(target_os = "macos")]
+	std::env::set_var("PROTOC", PathBuf::from("../../scripts/bin/mac/protoc"));
+	#[cfg(target_os = "linux")]
+	std::env::set_var("PROTOC", PathBuf::from("../../scripts/bin/lin/protoc"));
+	#[cfg(target_os = "windows")]
+	std::env::set_var("PROTOC", PathBuf::from("../../scripts/bin/win/protoc"));
 	tonic_build::configure().compile(&["../../proto/internal.proto", "../../proto/shared.proto"], &["../../proto/"])?;
 	Ok(())
 }
