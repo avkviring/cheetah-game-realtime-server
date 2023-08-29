9f5a9f1cb7e77a1b5f7f2f7cf06575e4fe17af70
Kviring Aleksey <alex@kviring.com>
2023-08-29 15:27:56 +0500

    chore(server): Тестирование и оптимизация dump команд

--- rust/Server/src/debug/mod.rs
+++ rust/Server/src/debug/mod.rs
@@ -5,6 +5,7 @@ use tokio::sync::Mutex;
 use tokio_stream::wrappers::TcpListenerStream;
 use warp::Filter;
 
+use crate::room::Room;
 use crate::server::manager::ServerManager;
 
 pub async fn run_debug_server(server_manager: Arc<Mutex<ServerManager>>, listener: TcpListener) {
@@ -28,34 +29,94 @@ fn get_help() -> String {
 }
 
 fn full_dump_rooms(server_manager: Arc<Mutex<ServerManager>>) -> String {
-	let server_manager = server_manager.try_lock().unwrap();
-	match server_manager.get_rooms() {
-		Ok(rooms) => {
-			let rooms: Vec<_> = rooms.into_iter().map(|id| server_manager.dump(id).unwrap()).collect();
-			match ron::to_string(&rooms) {
-				Ok(s) => s,
-				Err(e) => {
-					format!("Error {:?}", e)
-				}
-			}
-		}
-		Err(e) => format!("{:?}", e),
-	}
+	let mut result = String::new();
+	rooms_visitor(server_manager, |room| result.push_str(format!("{:?}", ron::to_string(&room)).as_str()));
+	result
 }
 
 fn simple_dump_rooms(server_manager: Arc<Mutex<ServerManager>>) -> String {
 	let mut result = String::new();
-	let server_manager = server_manager.try_lock().unwrap();
-	match server_manager.get_rooms() {
-		Ok(rooms) => {
-			let rooms: Vec<_> = rooms.into_iter().map(|id| server_manager.dump(id).unwrap()).collect();
-			for room in rooms {
-				if let Some(room) = room {
-					result.push_str(format!("{:?}-{:?}/n", room.id, room.members.len()).as_str())
+	rooms_visitor(server_manager, |room| result.push_str(format!("{:?}-{:?}/n", room.id, room.members.len()).as_str()));
+	result
+}
+
+fn rooms_visitor<OnRoom>(server_manager: Arc<Mutex<ServerManager>>, mut on_room: OnRoom) -> String
+where
+	OnRoom: FnMut(Room),
+{
+	let mut result = String::new();
+	let server_manager = server_manager.try_lock();
+	if let Ok(server_manager) = server_manager {
+		match server_manager.get_rooms() {
+			Ok(rooms) => {
+				let rooms: Vec<Room> = rooms.into_iter().map(|id| server_manager.dump(id)).filter_map(|item| item.ok()).filter_map(|item| item).collect();
+				for room in rooms {
+					on_room(room);
 				}
 			}
+			Err(e) => result.push_str(format!("{:?}", e).as_str()),
 		}
-		Err(e) => result.push_str(format!("{:?}", e).as_str()),
+	} else {
+		result.push_str("Cannot lock server_manager");
 	}
 	result
+}
+
+#[cfg(test)]
+pub mod test {
+	use std::sync::Arc;
+
+	use tokio::sync::Mutex;
+
+	use cheetah_common::network::bind_to_free_socket;
+	use cheetah_protocol::coniguration::ProtocolConfiguration;
+
+	use crate::debug::{full_dump_rooms, simple_dump_rooms};
+	use crate::room::template::config::RoomTemplate;
+	use crate::server::manager::ServerManager;
+
+	#[test]
+	pub fn test_simple_dump_rooms() {
+		let mut server_manager = ServerManager::new(
+			bind_to_free_socket().unwrap(),
+			ProtocolConfiguration {
+				disconnect_timeout: Default::default(),
+			},
+		)
+		.unwrap();
+		let room_id = server_manager
+			.create_room(RoomTemplate {
+				name: "".to_string(),
+				objects: vec![],
+				permissions: Default::default(),
+			})
+			.unwrap();
+
+		let server_manager = Arc::new(Mutex::new(server_manager));
+		let result = simple_dump_rooms(server_manager.clone());
+		assert_eq!(result, format!("{:?}-0/n", room_id))
+	}
+
+	#[test]
+	pub fn test_full_dump_rooms() {
+		let mut server_manager = ServerManager::new(
+			bind_to_free_socket().unwrap(),
+			ProtocolConfiguration {
+				disconnect_timeout: Default::default(),
+			},
+		)
+		.unwrap();
+		let room_id = server_manager
+			.create_room(RoomTemplate {
+				name: "perm_room".to_string(),
+				objects: vec![],
+				permissions: Default::default(),
+			})
+			.unwrap();
+
+		let server_manager = Arc::new(Mutex::new(server_manager));
+		let result = full_dump_rooms(server_manager);
+		assert!(result.contains("perm_room"));
+		assert!(result.contains(room_id.to_string().as_str()))
+	}
 }
