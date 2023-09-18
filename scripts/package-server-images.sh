aedc88a43e4ca8c3f32ad675e11686f102b35501
Kviring Alexey <alex@kviring.com>
2023-09-18 21:27:29 +0500

    Update package-server-images.sh

--- scripts/package-server-images.sh
+++ scripts/package-server-images.sh
@@ -9,5 +9,5 @@ set -e
 tempdir=$(mktemp -d)
 cp rust/grpc_health_probe $tempdir/grpc_health_probe
 cp -R rust/target/x86_64-unknown-linux-musl/release/cheetah-server $tempdir/cheetah-server
-docker build $tempdir -f rust/Server/Dockerfile -t ghcr.io/cheetah-game-platform/platform/cheetah-server:${version}
-docker push ghcr.io/cheetah-game-platform/platform/cheetah-server:${version}
+docker build $tempdir -f rust/Server/Dockerfile -t ghcr.io/avkviring/platform/cheetah-server:${version}
+docker push ghcr.io/avkviring/platform/cheetah-server:${version}
